b7099515a24a0af395dd5f4029cea5a0f2adb40a
JakeLD <jakeld@gmail.com>
2020-09-03 23:08:21 -0400

    Reorganize FSLogix files

--- Microsoft/FSLogix Apps/Start-Invoke-FslShrinkDisk.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-Expression "$PSScriptRoot\Invoke-FslShrinkDisk.ps1 -Path E:\FSLogix -Recurse -IgnoreLessThanGB 3 -DeleteOlderThanDays 90 -LogFilePath $PSScriptRoot\Invoke-FslShrinkDisk.csv -ThrottleLimit 20"
-Exit
