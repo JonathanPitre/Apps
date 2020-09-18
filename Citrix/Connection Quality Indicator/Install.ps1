cff1f5da0c62a5baf58508605f3dc302ff691e81
JakeLD <jakeld@gmail.com>
2020-09-18 09:28:59 -0400

    Add install script for Citrix Connection Quality Indicator

---
+++ Citrix/Connection Quality Indicator/Install.ps1
@@ -0,0 +1,138 @@
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2020 Jonathan Pitre, inspired by xenappblog.com
+# Example 1 Install EXE:
+# Execute-Process -Path .\appName.exe -Parameters "/silent"
+# Example 2 Install MSI:
+# Execute-MSI -Action Install -Path appName.msi -Parameters "/QB" -AddParameters "ALLUSERS=1"
+# Example 3 Uninstall MSI:
+# Remove-MSIApplications -Name "appName" -Parameters "/QB"
+
+#Requires -Version 5.1
+
+# Custom package providers list
+$PackageProviders = @("Nuget")
+
+# Custom modules list
+$Modules = @("PSADT", "Evergreen")
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
+# Checking for elevated permissions...
+If (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning -Message "Insufficient permissions to continue! PowerShell must be run with admin rights."
+    Break
+}
+Else {
+    Write-Verbose -Message "Importing custom modules..." -Verbose
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+
+    # Install custom package providers list
+    Foreach ($PackageProvider in $PackageProviders) {
+        If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
+    }
+
+    # Add the Powershell Gallery as trusted repository
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+    # Update PowerShellGet
+    $InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
+    $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
+    If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
+
+    # Install and import custom modules list
+    Foreach ($Module in $Modules) {
+        If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
+        Else {
+            $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
+            $ModuleVersion = (Find-Module -Name $Module).Version
+            $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
+            $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
+            If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion) {
+                Update-Module -Name $Module -Force
+                Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
+            }
+        }
+    }
+
+    Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
+}
+
+# Get the current script directory
+Function Get-ScriptDirectory {
+    Remove-Variable appScriptDirectory
+    Try {
+        If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
+        ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
+        ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
+        Else {
+            Write-Host -ForegroundColor Red "Cannot resolve script file's path"
+            Exit 1
+        }
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
+        Exit 2
+    }
+}
+
+# Variables Declaration
+# Generic
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$env:SEE_MASK_NOZONECHECKS = 1
+$appScriptDirectory = Get-ScriptDirectory
+
+# Application related
+##*===============================================
+$appVendor = "Citrix"
+$appName = "Connection Quality Indicator"
+$appProcess = @("Citrix.CQI")
+$appInstallParameters = "/quiet DISABLE_CEIP=1"
+#$Evergreen = Get-CitrixWorkspaceApp | Where-Object {$_.Title -contains "Citrix Workspace - Current Release"}
+$appVersion = (Get-ChildItem $appScriptDirectory | Where-Object { $_.PSIsContainer } | Sort-Object CreationTime -Descending | Select-Object -First 1 | Select-Object -ExpandProperty Name)
+$appURL = "https://support.citrix.com/supportkc/filedownload?uri=/filedownload/CTX220774/CitrixCQI.zip"
+$appSetup = "CitrixCQI.msi"
+$appSource = $appVersion
+$appDestination = "${env:ProgramFiles(x86)}\Citrix\Connection Quality Indicator"
+[boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName")
+$appInstalledVersion = ((Get-InstalledApplication -Name "$appVendor $appName").DisplayVersion)
+##*===============================================
+
+If ([version]$appVersion -gt [version]$appInstalledVersion) {
+    Set-Location -Path $appScriptDirectory
+    If (-Not(Test-Path -Path $appSource)) {New-Folder -Path $appSource}
+    Set-Location -Path $appSource
+
+    <#
+    If (-Not(Test-Path -Path $appScriptDirectory\$appSource\$appSetup)) {
+        Write-Log -Message "Downloading $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
+    }
+    Else {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+    #>
+
+    Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Get-Process -Name $appProcess | Stop-Process -Force
+
+    Write-Log -Message "Installing $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Execute-MSI -Action Install -Path $appSetup -Parameters $appInstallParameters
+
+    Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
+
+    Write-Log -Message "$appVendor $appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
+
+}
+Else {
+    Write-Log -Message "$appVendor $appName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
+}
+
+<#
+Write-Verbose -Message "Uninstalling custom modules..." -Verbose
+Foreach ($Module in $Modules) {
+    If ((Get-Module -ListAvailable -Name $Module)) {Uninstall-Module -Name $Module -Force}
+}
+Write-Verbose -Message "Custom modules were succesfully uninstalled!" -Verbose
+#>
