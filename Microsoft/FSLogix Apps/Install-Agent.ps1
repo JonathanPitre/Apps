289a90cfe1f510f5303ff576764ce5d3035c87f2
JakeLD <jakeld@gmail.com>
2020-09-22 00:43:47 -0400

    Add auto download of Invoke-FslShrinkDisk.ps1 Move policy definitions files in a structured folder Add logic to fix the Windows search settings depending on OS Autostart search service Add logic to detect AppLayering and fix altitude setting Add logic to detect Citrix Profile Management and disable it Add recommended antivirus exclusions Fix creation of Start Menu icon

--- Microsoft/FSLogix Apps/Install-Agent.ps1
+++ Microsoft/FSLogix Apps/Install-Agent.ps1
@@ -92,10 +92,11 @@ $appProcess = @("frxsvc", "frxtray", "frxshell", "frxccds")
 $appInstallParameters = "/install /quiet /norestart"
 $Evergreen = Get-MicrosoftFSLogixApps
 $appVersion = $Evergreen.Version
-$appURL = $Evergreen.uri
+$appURL = $Evergreen.URI
+$appURLScript = "https://raw.githubusercontent.com/FSLogix/Invoke-FslShrinkDisk/master/Invoke-FslShrinkDisk.ps1"
 $appZip = "FSLogix Apps.zip"
 $appSource = "$appVersion"
-$appDestination = "$envProgramFiles\FSLogix\Apps"
+$appDestination = "$env:ProgramFiles\FSLogix\Apps"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName" -Exact) | Select-Object -Last 1
 $appInstalledVersion = (Get-InstalledApplication -Name "$appVendor $appName" -Exact).DisplayVersion | Select-Object -Last 1
 ##*===============================================
@@ -109,11 +110,19 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
         Write-Log -Message "Downloading $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
         Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appZip
         Expand-Archive -Path $appZip -DestinationPath $appScriptDirectory\$appSource
+        # Move the policy definitions files
+        If (-Not(Test-Path -Path "$appScriptDirectory\PolicyDefinitions")) {New-Folder -Path "$appScriptDirectory\PolicyDefinitions"}
+        If (-Not(Test-Path -Path "$appScriptDirectory\PolicyDefinitions\en-US")) {New-Folder -Path "$appScriptDirectory\PolicyDefinitions\en-US"}
+        Move-Item -Path .\fslogix.admx -Destination "$appScriptDirectory\PolicyDefinitions\fslogix.admx" -Force
+        Move-Item -Path .\fslogix.adml -Destination "$appScriptDirectory\PolicyDefinitions\en-US\fslogix.adml" -Force
         Remove-File -Path $appZip
     }
     Else {
-        Write-Log -Message "File already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
+
+    # Download latest version of Jim Moyle's Invoke-FslShrinkDisk.ps1 script
+    Invoke-WebRequest -UseBasicParsing -Uri $appURLScript -OutFile "$appScriptDirectory\FileServer Maintenance\Invoke-FslShrinkDisk.ps1"
 
     Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
     Get-Process -Name $appProcess | Stop-Process -Force
@@ -122,19 +131,68 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
     Execute-Process -Path .\x64\Release\$appSetup -Parameters $appInstallParameters
 
     Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
-    # Configure Windows Search service auto-start
-    Set-ServiceStartMode -Name "WSearch" -StartMode "Automatic"
-    Start-ServiceAndDependencies -Name "WSearch"
-    # Windows Search CoreCount modification
-    Set-RegistryKey -Key "HKLM\SOFTWARE\Microsoft\Windows Search" -Name "CoreCount" -Type "Dword" -Value "1"
 
-    Register-ScheduledTask -Xml (Get-Content $appScriptDirectory\WSearch.xml | Out-String) -TaskName "Reset Windows Search at Logoff" -Force
+    # Add shortcut on the Start Menu
     New-Folder -Path "$envCommonStartMenuPrograms\Troubleshooting Tools" -ContinueOnError $True
     New-Shortcut -Path "$envCommonStartMenuPrograms\Troubleshooting Tools\FSLogix Tray Icon.lnk" -TargetPath "$appDestination\frxtray.exe" -IconLocation "$appDestination\frxtray.exe" -Description "FSLogix Tray Icon" -WorkingDirectory "$appDestination"
-    #Write-Log -Message "Adjusting altitude settings for FSLogix to play nice to CVAD UPL filter driver" -Severity 1 -LogType $LogType -WriteHost $True
-    #If ((Get-ItemProperty HKLM:\System\CurrentControlSet\Services\frxdrvvt\Instances\frxdrvvt -name Altitude) -ne 138010) {
-    #Set-RegistryKey -Key "HKLM:\System\CurrentControlSet\Services\frxdrvvt\Instances\frxdrvvt" -Name "Altitude" -Value "138010" -Type "String"
-    #}
+
+    # Enable FSLogix Apps agent search roaming - Apply different configurations based on operating system
+    If (-not(Get-ItemProperty -Path "HKLM:SOFTWARE\FSLogix\Apps" -Name "RoamSearch")) {
+        If ($envOSName -Like "*Windows Server 2012*" -or $envOSName -Like "*Windows Server 2016") {
+            # Install Windows Search feature when missing, if Office was installed before it must be repair!
+            If (!(Get-WindowsFeature -Name Search-Service)) {Install-WindowsFeature Search-Service}
+        }
+        If ($envOSName -Like "*Windows Server 201*" -or $envOSName -eq "Microsoft Windows 10 Enterprise for Virtual Desktops") {
+            # Limit Windows Search to a single cpu core - https://social.technet.microsoft.com/Forums/en-US/88725f57-67ed-4c09-8ae6-780ff785e555/problems-with-search-service-on-server-2012-r2-rds?forum=winserverTS
+            Set-RegistryKey -Key "HKLM\SOFTWARE\Microsoft\Windows Search" -Name "CoreCount" -Type "DWord" -Value "1"
+            # Configure multi-user search - https://docs.microsoft.com/en-us/fslogix/configure-search-roaming-ht
+            Set-RegistryKey -Key "HKLM\SOFTWARE\FSLogix\Apps" -Name "RoamSearch" -Type "DWord" -Value "2"
+            Set-RegistryKey -Key "HKLM\SOFTWARE\FSLogix\Profiles" -Name "RoamSearch" -Type "DWord" -Value "2"
+        }
+        If ($envOSName -Like "*Windows Server 2019*" -or $envOSName -eq "Microsoft Windows 10 Enterprise for Virtual Desktops") {
+            # Disable built-in Windnows per user search catalog since it's buggy - https://support.citrix.com/article/CTX270433
+            # https://jkindon.com/2020/03/15/windows-search-in-server-2019-and-multi-session-windows-10
+            Set-RegistryKey -Key "HKLM\SOFTWARE\Microsoft\Windows Search" -Name "EnablePerUserCatalog" -Value 0 -Type "DWord"
+        }
+        If ($envOSName -Like "*Windows 10*" -and $envOSName -ne "Microsoft Windows 10 Enterprise for Virtual Desktops") {
+            Set-RegistryKey -Key "HKLM\SOFTWARE\FSLogix\Apps" -Name "RoamSearch" -Type "DWord" -Value "1"
+            Set-RegistryKey -Key "HKLM\SOFTWARE\FSLogix\Profiles" -Name "RoamSearch" -Type "DWord" -Value "1"
+        }
+        Else {
+            Write-Log -Message "$appVendor $appNameSearch search roaming was enabled" -Severity 1 -LogType CMTrace -WriteHost $True
+        }
+    }
+
+    # Configure Windows Search service auto-start and start it
+    $serviceName = "WSearch"
+    If ((Get-ServiceStartMode -Name $serviceName) -ne "Automatic (Delayed Start)") {Set-ServiceStartMode -Name $serviceName -StartMode "Automatic (Delayed Start)"}
+    Start-ServiceAndDependencies -Name $serviceName
+
+    # Fix for Citrix App Layering and FSLogix integration, must be done in the platform layer - https://social.msdn.microsoft.com/Forums/windows/en-US/660959a4-f9a9-486b-8a0d-dec3eba549e3/using-citrix-app-layering-unidesk-with-fslogix?forum=FSLogix
+    # https://www.citrix.com/blogs/2020/01/07/citrix-app-layering-and-fslogix-profile-containers
+    If ((Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Services\unifltr") -and (Get-RegistryKey -Key "HKLM\SYSTEM\CurrentControlSet\Services\frxdrvvt\Instances\frxdrvvt" -Value "Altitude") -ne 138010) {
+        Write-Log -Message "Modifying $appVendor $appName altitude setting to be compatible with Citrix App Layering..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Set-RegistryKey -Key "HKLM\SYSTEM\CurrentControlSet\Services\frxdrvvt\Instances\frxdrvvt" -Name "Altitude" -Value "138010" -Type "String"
+    }
+
+    # Disable Citrix Profile Management if detected
+    If ((Test-Path -Path "HKLM\SYSTEM\CurrentControlSet\Services\ctxProfile") -and (Get-RegistryKey -Key "HKLM\SOFTWARE\Policies\Citrix\UserProfileManager" -Value "PSEnabled") -ne "0") {
+        Write-Log -Message "Disabling Citrix Profile Management..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Set-RegistryKey -Key "HKLM\SOFTWARE\Policies\Citrix\UserProfileManager" -Name "PSEnabled" -Value "0" -Type "DWord"
+    }
+
+    # Add Windows Defender exclusion(s) - https://docs.microsoft.com/en-us/azure/architecture/example-scenario/wvd/windows-virtual-desktop-fslogix
+    Add-MpPreference -ExclusionPath "%ProgramFiles%\FSLogix\Apps\frxdrv.sys" -Force
+    Add-MpPreference -ExclusionPath "%ProgramFiles%\FSLogix\Apps\frxdrvvt.sys" -Force
+    Add-MpPreference -ExclusionPath "%ProgramFiles%\FSLogix\Apps\frxccd.sys" -Force
+    Add-MpPreference -ExclusionPath "%TEMP%*.VHD" -Force
+    Add-MpPreference -ExclusionPath "%TEMP%*.VHDX" -Force
+    Add-MpPreference -ExclusionPath "%windir%\TEMP*.VHD" -Force
+    Add-MpPreference -ExclusionPath "%windir%\TEMP*.VHDX" -Force
+    Add-MpPreference -ExclusionProcess "%ProgramFiles%\FSLogix\Apps\frxsvc.exe" -Force
+    Add-MpPreference -ExclusionProcess "%ProgramFiles%\FSLogix\Apps\frxccds.exe" -Force
+    Add-MpPreference -ExclusionProcess "%ProgramFiles%\FSLogix\Apps\frxsvc.exe" -Force
+    #Add-MpPreference -ExclusionProcess "%ProgramFiles%\FSLogix\Apps\frx.exe" -Force
 
     Write-Log -Message "$appVendor $appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 
@@ -143,8 +201,10 @@ Else {
     Write-Log -Message "$appVendor $appName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
 
-#Write-Verbose -Message "Uninstalling custom modules..." -Verbose
-#Foreach ($Module in $Modules) {
-#    If ((Get-InstalledModule -Name $Module)) {Uninstall-Module -Name $Module -Force}
-#    Write-Verbose -Message "Custom modules were uninstalled!" -Verbose
-#}
+<#
+Write-Verbose -Message "Uninstalling custom modules..." -Verbose
+Foreach ($Module in $Modules) {
+    If ((Get-InstalledModule -Name $Module)) {Uninstall-Module -Name $Module -Force}
+    Write-Verbose -Message "Custom modules were uninstalled!" -Verbose
+}
+#>
