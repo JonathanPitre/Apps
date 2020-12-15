16490ce6b7231688775cab27548d77541bf7fdbb
JakeLD <jakeld@gmail.com>
2020-12-14 21:33:14 -0500

    Add policy definitions download Add missing service Improve uninstallation routine

--- Google/Chrome Enterprise/Install.ps1
+++ Google/Chrome Enterprise/Install.ps1
@@ -87,34 +87,58 @@ $appScriptDirectory = Get-ScriptDirectory
 ##*===============================================
 $appVendor = "Google"
 $appName = "Chrome"
-$appLongName = "Chrome Enterprise"
+$appLongName = "Enterprise"
 $appSetup = "GoogleChromeStandaloneEnterprise64.msi"
-$appProcess = @("chrome","GoogleUpdate")
+$appProcesses = @("chrome", "GoogleUpdate", "chrome_proxy", "elevation_service")
+$appServices = @("gupdate", "gupdatem", "GoogleChromeElevationService")
 $appInstallParameters = "/QB"
 $Evergreen = Get-GoogleChrome | Where-Object {$_.Architecture -eq "x64"}
 $appVersion = $Evergreen.Version
 $appURL = $Evergreen.URI
-$appURLADMX = "https://chromeenterprise.google/browser/download/thank-you/?platform=ADMADMX&channel=stable&usagestats=0#"
+$appURLADMX = "https://dl.google.com/dl/edgedl/chrome/policy/policy_templates.zip"
+$appADMX = ($appURLADMX).Split("/")[7]
+$appURLADMX2 = "https://dl.google.com/dl/update2/enterprise/googleupdateadmx.zip"
+$appADMX2 = ($appURLADMX2).Split("/")[6]
 $appSource = $appVersion
-$appDestination = "${env:ProgramFiles(x86)}\$appVendor\$appName\Application"
+$appDestination = "$env:ProgramFiles\$appVendor\$appName\Application"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName" -Exact)
 $appInstalledVersion = (Get-InstalledApplication -Name "$appVendor $appName" -Exact).DisplayVersion | Select-Object -First 1
 ##*===============================================
 
 If ([version]$appVersion -gt [version]$appInstalledVersion) {
     Set-Location -Path $appScriptDirectory
-    If (-Not(Test-Path -Path $appSource)) {New-Folder -Path $appSource}
+    If (-Not(Test-Path -Path $appSource)) { New-Folder -Path $appSource }
     Set-Location -Path $appSource
 
+    # Download latest file installer
     If (-Not(Test-Path -Path $appScriptDirectory\$appSource\$appSetup)) {
-        Write-Log -Message "Downloading $appVendor $appLongName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Write-Log -Message "Downloading $appVendor $appName $appLongName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
         Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
     }
     Else {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
-    Get-Process -Name $appProcess | Stop-Process -Force
+    # Download latest policy definitions
+    If (-Not(Test-Path -Path $appScriptDirectory\PolicyDefinitions\*.admx)) {
+        Write-Log -Message "Downloading $appVendor $appName $appLongName $appVersion ADMX templates..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLADMX -OutFile $appScriptDirectory\$appADMX
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLADMX2 -OutFile $appScriptDirectory\$appADMX2
+        New-Folder -Path "$appScriptDirectory\PolicyDefinitions"
+        If (Get-ChildItem -Path $appScriptDirectory -Filter *.zip) {
+            Get-ChildItem -Path $appScriptDirectory -Filter *.zip | Expand-Archive -DestinationPath $appScriptDirectory\PolicyDefinitions -Force
+            Remove-File -Path $appScriptDirectory\*.zip -ContinueOnError $True
+        }
+        Move-Item -Path $appScriptDirectory\PolicyDefinitions\GoogleUpdateAdmx\* -Destination $appScriptDirectory\PolicyDefinitions -Force
+        Move-Item -Path $appScriptDirectory\PolicyDefinitions\windows\admx\* -Destination $appScriptDirectory\PolicyDefinitions -Force
+        Remove-Item -Path $appScriptDirectory\PolicyDefinitions -Include "GoogleUpdateAdmx", "chromeos", "common", "windows", "VERSION" -Force -Recurse
+    }
+    Else {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    Get-Process -Name $appProcesses | Stop-Process -Force
+
     # Delete machine policies to prevent issue during installation
     Remove-RegistryKey -Key "HKLM:\SOFTWARE\Policies\$appVendor\Update" -Recurse -ContinueOnError $True
     Remove-RegistryKey -Key "HKLM:\SOFTWARE\Policies\$appVendor\$appName" -Recurse -ContinueOnError $True
@@ -122,43 +146,72 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
     Remove-RegistryKey -Key "HKLM:\SOFTWARE\WOW6432Node\Policies\$appVendor\Update" -Recurse -ContinueOnError $True
     Remove-RegistryKey -Key "HKLM:\SOFTWARE\WOW6432Node\Policies\$appVendor\$appName" -Recurse -ContinueOnError $True
 
+    # Uninstall previous versions
     If ($IsAppInstalled) {
         Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Remove-MSIApplications -Name $appName -Parameters $appInstallParameters
-        Remove-MSIApplications -Name $($appName)Update -Parameters $appInstallParameters
+        Remove-MSIApplications -Name "$appVendor $appName" -Parameters $appInstallParameters
+        Remove-MSIApplications -Name $($appVendor)Update -Parameters $appInstallParameters
     }
 
-    If (Test-Path -Path "$envProgramFilesX86\$($appName)Update") {
-        Write-Log -Message "Removing previous $appVendor $appLongName folder to fix issues with new installation." -Severity 1 -LogType CMTrace -WriteHost $True
-        Set-Location -Path "$envProgramFilesX86\$appVendor\$($appName)Update"
-        Execute-Process -Path ".\$appVendor$($appName)Update.exe" -Parameters "-uninstall" -IgnoreExitCodes 1606220281 -ContinueOnError $True
+    # Uninstall Google Update
+    If (Test-Path -Path "$env%LocalAppData\$appVendor\Update\$($appVendor)Update.exe") {
+        Write-Log -Message "Removing previous $appVendor $appName $appLongName folder to fix issues with new installation." -Severity 1 -LogType CMTrace -WriteHost $True
+        Execute-Process -Path "$envLocalAppData\$appVendor\Update\$($appVendor)Update.exe" -Parameters "-uninstall" -IgnoreExitCodes 1606220281 -ContinueOnError $True
     }
+    If (Test-Path -Path "$envProgramFilesX86\$appVendor\Update\$($appVendor)Update.exe") {
+        Write-Log -Message "Removing previous $appVendor $appName $appLongName folder to fix issues with new installation." -Severity 1 -LogType CMTrace -WriteHost $True
+        Execute-Process -Path "$envProgramFilesX86\$appVendor\Update\$($appVendor)Update.exe" -Parameters "-uninstall" -IgnoreExitCodes 1606220281 -ContinueOnError $True
+    }
+
+    # Remove previous install folders
+    Remove-Folder -Path "$envLocalAppData\$appVendor\$appName" -ContinueOnError $True
+    Remove-Folder -Path "$envLocalAppData\$appVendor\Update" -ContinueOnError $True
+    Remove-Folder -Path "$envLocalAppData\$appVendor\Software Reporter Tool" -ContinueOnError $True
+    Remove-Folder -Path "$envLocalAppData\$appVendor\Policies" -ContinueOnError $True
+    Remove-Folder -Path "$envLocalAppData\$appVendor\Temp" -ContinueOnError $True
+    Remove-Folder -Path "$envLocalAppData\$appVendor\CrashReports" -ContinueOnError $True
     Remove-Folder -Path "$envProgramFilesX86\$appVendor\$appName" -ContinueOnError $True
-    Remove-Folder -Path "$envProgramFilesX86\$appVendor\$($appName)Update" -ContinueOnError $True
+    Remove-Folder -Path "$envProgramFilesX86\$appVendor\Update" -ContinueOnError $True
+    Remove-Folder -Path "$envProgramFilesX86\$appVendor\Policies" -ContinueOnError $True
     Remove-Folder -Path "$envProgramFilesX86\$appVendor\Temp" -ContinueOnError $True
-    Write-Log -Message "Installing $appVendor $appLongName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Remove-Folder -Path "$envProgramFilesX86\$appVendor\CrashReports" -ContinueOnError $True
+
+    Write-Log -Message "Installing $appVendor $appName $appLongName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
     Execute-MSI -Action Install -Path $appSetup -Parameters $appInstallParameters
 
     Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
+
+    # Copy preferences file
     Copy-File -Path "$appScriptDirectory\master_preferences" -Destination $appDestination
+
+    # Stop and disable unneeded scheduled tasks
     Get-ScheduledTask -TaskName "$appVendor*" | Stop-ScheduledTask
     Get-ScheduledTask -TaskName "$appVendor*" | Disable-ScheduledTask
-    Stop-ServiceAndDependencies -Name gupdate
-    Set-ServiceStartMode -Name gupdate -StartMode "Disabled"
-    Stop-ServiceAndDependencies -Name gupdatem
-    Set-ServiceStartMode -Name gupdatem -StartMode "Disabled"
+
+    # Stop and disable unneeded services
+    Stop-ServiceAndDependencies -Name $appServices[0]
+    Stop-ServiceAndDependencies -Name $appServices[1]
+    Stop-ServiceAndDependencies -Name $appServices[2]
+    Set-ServiceStartMode -Name $appServices[0] -StartMode "Disabled"
+    Set-ServiceStartMode -Name $appServices[1] -StartMode "Disabled"
+    Set-ServiceStartMode -Name $appServices[3] -StartMode "Disabled"
+
     # Creates a pinned taskbar icons for all users
-    New-Shortcut -Path "$envSystemDrive\Users\Default\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\Taskbar\$appVendor $appName.lnk" -TargetPath "$appDestination\$($appProcess[0]).exe"  -IconLocation "$appDestination\$($appProcess[0]).exe" -Description "$appVendor $appName" -WorkingDirectory "$appDestination"
-    # Remove Active Setup -https://dennisspan.com/google-chrome-on-citrix-deep-dive/#StubPath
+    New-Shortcut -Path "$envSystemDrive\Users\Default\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\Taskbar\$appVendor $appName.lnk" -TargetPath "$appDestination\$($appProcesses[0]).exe"  -IconLocation "$appDestination\$($appProcesses[0]).exe" -Description "$appVendor $appName" -WorkingDirectory "$appDestination"
+    
+    # Remove Active Setup - https://dennisspan.com/google-chrome-on-citrix-deep-dive/#StubPath
     Remove-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{8A69D345-D564-463c-AFF1-A69D9E530F96}" -Name "StubPath"
+
+    # Remove desktop shortcut for all users
     #Remove-File "$envCommonDesktop\$appVendor $appName.lnk" -ContinueOnError $True
+
     Update-GroupPolicy
 
-    Write-Log -Message "$appVendor $appLongName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
+    Write-Log -Message "$appVendor $appName $appLongName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 
 }
 Else {
-    Write-Log -Message "$appVendor $appLongName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
+    Write-Log -Message "$appVendor $appName $appLongName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
 
 <#
