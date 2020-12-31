e941425043c287e8d30259a8c886680f82d448c3
JakeLD <jakeld@gmail.com>
2020-12-31 06:05:46 -0500

    Add customization wizard dowload Add automatic policy definitions download Add automatic patch download Add warning for Adobe Creative Cloud Improve scheduled tasks Fix application installed detection

--- Adobe/Acrobat DC/Install.ps1
+++ Adobe/Acrobat DC/Install.ps1
@@ -87,50 +87,101 @@ $appScriptDirectory = Get-ScriptDirectory
 ##*===============================================
 $appVendor = "Adobe"
 $appName = "Acrobat"
-$appSetup = "AcroPro.msi"
-$appProcess = @("Acrobat", "AcroBroker", "acrobat_sl", "AcroCEF", "acrodist", "acrotray", "AcroRd32", "Acrobat Elements", "AcroTextExtractor", "ADelRCP", "AdobeCollabSync", "arh", "FullTrustNotIfier", "LogTransport2", "wow_helper", "outlook", "chrome", "iexplore")
-$appTransform = "AcroPro.mst"
-$appInstallParameters = "/QB"
-$appParameters = "--tool=VolumeSerialize --generate --serial=1016-1899-8440-6413-0576-7429 --leid=V7{}AcrobatCont-12-Win-GM --regsuppress=ss --eulasuppress --stream" #--provfile Optional; path of the folder where prov.xml is created. If this parameter is not specified, prov.xml is created in the folder in which APTEE resides.
-$appAddParameters = "IGNOREVCRT64=1 EULA_ACCEPT=YES UPDATE_MODE=0 DISABLE_ARM_SERVICE_INSTALL=1 ROAMIDENTITY=1 ROAMLICENSING=1"
-$appVersion = ($appPatch).Substring(12, 10)
-$appVersion = ("$appVersion").Insert(2, ".").Insert(6, ".")
 $appShortVersion = "DC"
-$appPatch = (Get-ChildItem $appScriptDirectory | Where-Object -Property Name -Match -Value "AcrobatDCUpd.*.msp$" | Sort-Object CreationTime -Descending | Select-Object -First 1 | Select-Object -ExpandProperty Name)
+$appProcesses = @("Acrobat", "AcroBroker", "acrobat_sl", "AcroCEF", "acrodist", "acrotray", "AcroRd32", "Acrobat Elements", "AcroTextExtractor", "ADelRCP", "AdobeCollabSync", "arh", "FullTrustNotIfier", "LogTransport2", "wow_helper", "outlook", "chrome", "iexplore")
+$appServices = @("AdobeUpdateService")
+$appTransform = "AcroPro.mst"
+$appSetup = "AcroPro.msi"
+$appInstallParameters = "/QB"
+#$appParameters = "--tool=VolumeSerialize --generate --serial=1016-1899-8440-6413-0576-7429 --leid=V7{}AcrobatCont-12-Win-GM --regsuppress=ss --eulasuppress --stream" #--provfile Optional; path of the folder where prov.xml is created. If this parameter is not specified, prov.xml is created in the folder in which APTEE resides.
+$appAddParameters = "IGNOREVCRT64=1 EULA_ACCEPT=YES UPDATE_MODE=0 DISABLE_ARM_SERVICE_INSTALL=1 ROAMIDENTITY=1 ROAMLICENSING=1"
+$Evergreen = Get-AdobeAcrobatProDC
+$appVersion = $Evergreen.Version
+$appURLPatch = $Evergreen.URI
+$appPatch = ($appURLPatch).Split("/")[9]
+$appURLADMX = "ftp://ftp.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/AcrobatADMTemplate.zip"
+$appADMX = ($appURLADMX).Split("/")[9]
+$appURLCustWiz = "ftp://ftp.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/CustWiz2000920067_en_US_DC.exe"
+$appCustWiz = ($appURLCustWiz).Split("/")[9]
+$appCustWizVersion = $appCustWiz.Trim("CustWiz").Trim("_en_US_DC.exe")
 $appSource = $appVersion
 $appDestination = "${env:ProgramFiles(x86)}\$appVendor\$appName $appShortVersion\$appName"
-[boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName $appShortVersion")
+[boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName $appShortVersion" -Exact)
 $appInstalledVersion = (Get-InstalledApplication -Name "$appVendor $appName $appShortVersion").DisplayVersion
 ##*===============================================
 
 If ([version]$appVersion -gt [version]$appInstalledVersion) {
     Set-Location -Path $appScriptDirectory
 
-    Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
-    Get-Process -Name $appProcess | Stop-Process -Force
+    # Uninstall previous versions
+    Get-Process -Name $appProcesses | Stop-Process -Force
 
-    Write-Log -Message "Installing $appVendor $appName $appShortVersion $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-    Execute-MSI -Action Install -Path $appSetup -Transform $appTransform -Parameters $appInstallParameters -AddParameters $appAddParameters -Patch $appPatch -SkipMSIAlreadyInstalledCheck
+    # Download latest Adobe Acrobat Customization Wizard DC
+    If (-Not(Test-Path -Path $appScriptDirectory\$appCustWiz)) {
+        Write-Log -Message "Downloading $appVendor $appName Custimization Wizard $appShortVersion $appCustWizVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLCustWiz -OutFile $appCustWiz
+    }
+    Else {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    # Download latest policy definitions
+    Write-Log -Message "Downloading $appVendor $appName $appShortVersion ADMX templates..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Invoke-WebRequest -UseBasicParsing -Uri $appURLADMX -OutFile $appADMX
+    New-Folder -Path "$appScriptDirectory\PolicyDefinitions"
+    Expand-Archive -Path $appADMX -DestinationPath "$appScriptDirectory\PolicyDefinitions" -Force
+    Remove-File -Path $appADMX, $appScriptDirectory\PolicyDefinitions\*.adm
+
+    # Download latest patch file
+    If (-Not(Test-Path -Path $appScriptDirectory\$appPatch)) {
+        Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appVersion patch..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLPatch -OutFile $appPatch
+        If (Test-Path -Path $appScriptDirectory\$appPatch) {
+            Set-IniValue -FilePath $appScriptDirectory\setup.ini -Section "Startup" -Key "CmdLine" -Value "/sPB /rs"
+            Set-IniValue -FilePath $appScriptDirectory\setup.ini -Section "Product" -Key "CmdLine" -Value "TRANSFORMS=`"$appTransform`""
+            Set-IniValue -FilePath $appScriptDirectory\setup.ini -Section "Product" -Key "PATCH" -Value $appPatch
+        }
+    }
+    Else {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    # Install latest version
+    If ((Test-Path -Path $appScriptDirectory\$appSetup) -and (Test-Path -Path $appScriptDirectory\$appTransform)) {
+        Write-Log -Message "Installing $appVendor $appName $appShortVersion $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Execute-MSI -Action Install -Path $appSetup -Transform $appTransform -Parameters $appInstallParameters -AddParameters $appAddParameters -Patch $appPatch -SkipMSIAlreadyInstalledCheck
+    }
+    Else {
+        Write-Log -Message "Setup file(s) are missing." -Severity 1 -LogType CMTrace -WriteHost $True
+        Exit-Script
+    }
 
     Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
-    Get-ScheduledTask -TaskName "$appVendor Acrobat Update Task" | Stop-ScheduledTask
-    Get-ScheduledTask -TaskName "$appVendor Acrobat Update Task" | Disable-ScheduledTask
-    Stop-ServiceAndDependencies -Name "$($appVendor)UpdateService"
-    Set-ServiceStartMode -Name "$($appVendor)UpdateService" -StartMode "Disabled"
-    #Stop-ServiceAndDependencies -Name "$($appVendor)ARMService"
-    #Set-ServiceStartMode -Name "$($appVendor)ARMService" -StartMode "Disabled"
-    Stop-ServiceAndDependencies -Name "$($appVendor)GCInvoker-1.0"
-    Set-ServiceStartMode -Name "$($appVendor)GCInvoker-1.0" -StartMode "Disabled"
+
+    # Stop and disable unneeded scheduled tasks
+    Get-ScheduledTask -TaskName "$appVendor $appName Update Task" | Stop-ScheduledTask
+    Get-ScheduledTask -TaskName "$appVendor $appName Update Task" | Disable-ScheduledTask
+
+    # Stop and disable unneeded services
+    Stop-ServiceAndDependencies -Name $appServices[0]
+    Set-ServiceStartMode -Name $appServices[0] -StartMode "Disabled"
+
+    # Remove unneeded applications from running at start-up
     Remove-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AdobeAAMUpdater-1.0" -ContinueOnError $True
     #Remove-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AdobeGCInvoker-1.0" -ContinueOnError $True
+
     <# Needed for serial number install only
     Execute-Process -Path ".\adobe_prtk.exe" -Parameters $appParameters #logs located in %temp%\AdobeSerialization.log, and %temp%\oobelib.log
     Execute-Process -Path "$appDestination\Acrobat.exe" -WindowStyle Minimized -NoWait
     Start-Sleep -s 70
-    Get-Process -Name $appProcess | Stop-Process -Force
-    Write-Verbose -Message "$appVendor $appName $appVersion was successfully installed!" -Verbose
-    Write-Log -Message "$appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
+    Get-Process -Name $appProcesses | Stop-Process -Force
     #>
+
+    Write-Log -Message "$appVendor $appName $appShortVersion $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
+    If (-Not(Get-InstalledApplication -Name "Adobe Creative Cloud")) {
+        Write-Log -Message "Adobe Creative Cloud must be installed in order for $appVendor $appName $appShortVersion licensing to work!" -Severity 2 -LogType CMTrace -WriteHost $True
+    }
+
 }
 Else {
     Write-Log -Message "$appVendor $appName $appShortVersion $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
