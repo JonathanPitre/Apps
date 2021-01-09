41b0bf973ab7d9e5e57ed88ee1a0b4258d83afab
JakeLD <jakeld@gmail.com>
2021-01-08 22:04:04 -0500

    Add automatic download of latest patches

--- Druide/Antidote/Install.ps1
+++ Druide/Antidote/Install.ps1
@@ -87,50 +87,91 @@ $appScriptDirectory = Get-ScriptDirectory
 ##*===============================================
 $appVendor = "Druide"
 $appName = "Antidote"
-$appShortVersion = "10"
-$appVersion = Get-ChildItem $appScriptDirectory | Where-Object { $_.PSIsContainer } | Sort-Object CreationTime -Descending | Select-Object -First 1 | Select-Object -ExpandProperty Name
-$appSource = $appVersion
-$appSetup = "Antidote10.msi"
-$appSetup2 = "Antidote10-Module-francais.msi"
-$appSetup3 = "Antidote10-English-module.msi"
-$appSetup4 = "Antidote-Connectix10.msi"
+$appProcesses = @("Antidote", "AgentAntidote", "Connectix", "AgentConnectix", "OUTLOOK", "WINWORD", "EXCEL", "POWERPNT")
 $appTransform = "ReseauAntidote.mst"
 $appTransform2 = "ReseauConnectix.mst"
-$appPatch = "Diff_Antidote_10_C_10.4.msp"
-$appPatch2 = "Diff_Antidote_10_Module_F_10.4.msp"
-$appPatch3 = "Diff_Antidote_10_Module_E_10.4.msp"
-$appPatch4 = "Diff_Connectix_10_C_10.4.msp"
+$appURLVersion = "https://www.antidote.info/en/assistance/mises-a-jour/historique/antidote-10/windows"
+$webRequest = Invoke-WebRequest -UseBasicParsing -Uri ($appURLVersion) -SessionVariable websession
+$regexAppVersion = "Antidote \d\d v\d.+ Windows"
+$webVersion = $webRequest.RawContent | Select-String -Pattern $regexAppVersion -AllMatches | ForEach-Object { $_.Matches.Value } | Select-Object -First 1
+$appShortVersion = ($webVersion).Split(" ")[1]
+$appPatchVersion = ($webVersion).Trim("Andidote $appShortVersion v").Trim(" Windows").Replace(" ", "")
+$appVersion = "$appShortVersion.$appPatchVersion"
+$appURLPatch = "https://telechargement12.druide.com/Win/antidote_$appShortVersion/Diff_Antidote_$($appShortVersion)_C_$($appShortVersion).$appPatchVersion.msp"
+$appURLPatch2 = "https://telechargement12.druide.com/Win/antidote_$appShortVersion/Diff_Antidote_$($appShortVersion)_Module_F_$($appShortVersion).$appPatchVersion.msp"
+$appURLPatch3 = "https://telechargement12.druide.com/Win/antidote_$appShortVersion/Diff_Antidote_$($appShortVersion)_Module_E_$($appShortVersion).$appPatchVersion.msp"
+$appURLPatch4 = "https://telechargement12.druide.com/Win/antidote_$appShortVersion/Diff_Connectix_$($appShortVersion)_C_$($appShortVersion).$appPatchVersion.msp"
+$appPatch = $appURLPatch.Split("/")[5]
+$appPatch2 = $appURLPatch2.Split("/")[5]
+$appPatch3 = $appURLPatch3.Split("/")[5]
+$appPatch4 = $appURLPatch4.Split("/")[5]
+$appSetup = "Antidote$appShortVersion.msi"
+$appSetup2 = "Antidote$appShortVersion-Module-francais.msi"
+$appSetup3 = "Antidote$appShortVersion-English-module.msi"
+$appSetup4 = "Antidote-Connectix$appShortVersion.msi"
 $appInstallParameters = "/QB"
-$appProcesses = @("Antidote", "AgentAntidote", "Connectix", "AgentConnectix", "OUTLOOK", "WINWORD", "EXCEL", "POWERPNT")
+$appSource = $appPatchVersion
 $appDestination = "${env:ProgramFiles(x86)}\$appVendor\$appName $appShortVersion\Application\Bin64"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appName \d{2}" -RegEx) | Select-Object -First 1
 $appInstalledVersion = (Get-InstalledApplication -Name "$appName \d{2}" -RegEx).DisplayVersion | Select-Object -First 1
 ##*===============================================
 
 If ([version]$appVersion -gt [version]$appInstalledVersion) {
-
     Set-Location -Path $appScriptDirectory
+    If (-Not(Test-Path -Path $appSource)) { New-Folder -Path $appSource }
 
-    If (-Not(Test-Path -Path $appScriptDirectory\$appSource\$appSetup)) {
-        Write-Log -Message "Setup files are missing.Please download and try again." -Severity 1 -LogType CMTrace -WriteHost $True
-        Break
-    }
-    Else {
-        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
-        Set-Location -Path $appSource
+    # Check if setup file exist
+    If (-Not(Test-Path -Path $appScriptDirectory\$appSetup)) {
+        Write-Log -Message "Setup files are missing. Please download and try again." -Severity 1 -LogType CMTrace -WriteHost $True
+        Exit
     }
 
+    # Uninstall previous versions
     Get-Process -Name $appProcesses | Stop-Process -Force
     If ($IsAppInstalled) {
         Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
         Remove-MSIApplications -Name $appName -Parameters $appInstallParameters
     }
 
+    # Download latest setup file(s)
+    If (-Not(Test-Path -Path $appScriptDirectory\$appSource\$appPatch)) {
+        Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appVersion Patch..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLPatch -OutFile $appScriptDirectory\$appSource\$appPatch
+    }
+    Else {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    If (-Not(Test-Path -Path $appScriptDirectory\$appSource\$appPatch2)) {
+        Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appVersion Patch..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLPatch2 -OutFile $appScriptDirectory\$appSource\$appPatch2
+    }
+
+    Else {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+    If (-Not(Test-Path -Path $appScriptDirectory\$appSource\$appPatch3)) {
+        Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appVersion Patch..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLPatch3 -OutFile $appScriptDirectory\$appSource\$appPatch3
+    }
+    Else {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    If (-Not(Test-Path -Path $appScriptDirectory\$appSource\$appPatch4)) {
+        Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appVersion Patch..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLPatch4 -OutFile $appScriptDirectory\$appSource\$appPatch4
+    }
+    Else {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    # Install latest version
     Write-Log -Message "Installing $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-    Execute-MSI -Action Install -Path $appSetup -Parameters $appInstallParameters -Transform $appTransform -SkipMSIAlreadyInstalledCheck #-Patch $appPatch
-    Execute-MSI -Action Install -Path $appSetup2 -Parameters $appInstallParameters -SkipMSIAlreadyInstalledCheck #-Patch $appPatch2
-    If (Test-Path -Path $appSetup3) {Execute-MSI -Action Install -Path $appSetup3 -Parameters $appInstallParameters -SkipMSIAlreadyInstalledCheck} #-Patch $appPatch3
-    Execute-MSI -Action Install -Path $appSetup4 -Parameters $appInstallParameters -Transform $appTransform2 -SkipMSIAlreadyInstalledCheck #-Patch $appPatch4
+    Execute-MSI -Action Install -Path $appSetup -Parameters $appInstallParameters -Transform $appTransform -SkipMSIAlreadyInstalledCheck -Patch $appScriptDirectory\$appSource\$appPatch
+    Execute-MSI -Action Install -Path $appSetup2 -Parameters $appInstallParameters -SkipMSIAlreadyInstalledCheck -Patch $appScriptDirectory\$appSource\$appPatch2
+    If (Test-Path -Path $appSetup3) {Execute-MSI -Action Install -Path $appSetup3 -Parameters $appInstallParameters -SkipMSIAlreadyInstalledCheck} -Patch $appScriptDirectory\$appSource\$appPatch3
+    Execute-MSI -Action Install -Path $appSetup4 -Parameters $appInstallParameters -Transform $appTransform2 -SkipMSIAlreadyInstalledCheck -Patch $appScriptDirectory\$appSource\$appPatch4
 
     Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
 
