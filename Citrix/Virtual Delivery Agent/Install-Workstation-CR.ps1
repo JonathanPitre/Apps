2ac422146565b95322ffc3877d0ea3e5e64a28be
Jonathan Pitre <jakeld@gmail.com>
2021-04-27 00:29:48 -0400

    Update $appInstallParameters for version 2103 Enable MtuDiscovery reg fix Suppress reboot dialogox box Rename function Get-CitrixBinary to Get-CitrixDownload Change $appInstallParameters for version 2103

--- Citrix/Virtual Delivery Agent/Install-Workstation-CR.ps1
+++ Citrix/Virtual Delivery Agent/Install-Workstation-CR.ps1
@@ -77,36 +77,143 @@ $appName2 = "Virtual Delivery Agent"
 $appProcesses = @("BrokerAgent", "picaSessionAgent")
 # https://docs.citrix.com/en-us/citrix-virtual-apps-desktops-service/install-configure/install-command.html
 # https://docs.citrix.com/en-us/citrix-virtual-apps-desktops/install-configure/install-vdas-sccm.html
-$appInstallParameters = '/baseimage /noreboot /quiet /enable_remote_assistance /disableexperiencemetrics /virtualmachine /INSTALL_MCSIO_DRIVER /noresume /enable_real_time_transport /enable_hdx_ports /enable_hdx_udp_ports /exclude "User Personalization layer", "Citrix Files for Outlook", "Citrix Files for Windows", "Citrix Supportability Tools", "Citrix Telemetry Service", "Citrix Personalization for App-V - VDA", "Personal vDisk" /components vda /mastermcsimage'
-#$Evergreen = Get-EvergreenApp -Name CitrixVirtualAppsDesktopsFeed | Where-Object {$_.Title -contains "Citrix Virtual Apps and Desktops 7 2006, All Editions"}
-$appVersion = (Get-ChildItem $appScriptDirectory | Where-Object { $_.PSIsContainer } | Sort-Object CreationTime -Descending | Select-Object -First 1 | Select-Object -ExpandProperty Name)
-#$appURL = $Evergreen.URI
+$appInstallParameters = '/noreboot /quiet /enable_remote_assistance /disableexperiencemetrics /remove_appdisk_ack /remove_pvd_ack /virtualmachine /noresume /enable_real_time_transport /enable_hdx_ports /enable_hdx_udp_ports /components vda /mastermcsimage /includeadditional "Machine Identity Service","Citrix User Profile Manager","Citrix User Profile Manager WMI Plugin","Citrix MCS IODriver" /exclude "Workspace Environment Management","User Personalization layer","Citrix Files for Outlook","Citrix Files for Windows","Citrix Supportability Tools","Citrix Telemetry Service","Citrix Personalization for App-V - VDA","Personal vDisk"'
+$Evergreen = Get-EvergreenApp -Name CitrixVirtualAppsDesktopsFeed | Where-Object {$_.Title -like "Citrix Virtual Apps and Desktops 7 *, All Editions"} | Select-Object -First 1
+$appVersion = $Evergreen.Version
 $appSetup = "VDAWorkstationSetup_$appVersion.exe"
+$appDlNumber = "19145"
 $appDestination = "$env:ProgramFiles\$appVendor\Virtual Delivery Agent"
+$appHardwarePlatform = Get-HardwarePlatform
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor .*$appName2.*" -RegEx)
 $appInstalledVersion = (((Get-InstalledApplication -Name "$appVendor .*$appName2.*" -RegEx).DisplayVersion)).Substring(0, 4)
 ##*===============================================
+
+Function Get-CitrixDownload {
+    <#
+.SYNOPSIS
+  Downloads a Citrix VDA or ISO from Citrix.com utilizing authentication
+.DESCRIPTION
+  Downloads a Citrix VDA or ISO from Citrix.com utilizing authentication
+  Ryan Butler 2/6/2020 https://github.com/ryancbutler/Citrix/tree/master/XenDesktop/AutoDownload
+.PARAMETER dlNumber
+  Number assigned to binary download
+.PARAMETER dlEXE
+  File to be downloaded
+.PARAMETER dlPath
+  Path to store downloaded file. Must contain following slash (C:\Temp\)
+.PARAMETER CitrixUserName
+  Citrix.com username
+.PARAMETER CitrixPassword
+  Citrix.com password
+.EXAMPLE
+  Get-CitrixDownload -dlNumber "16834" -dlEXE "Citrix_Virtual_Apps_and_Desktops_7_1912.iso" -CitrixUserName "MyCitrixUsername" -CitrixPassword "MyCitrixPassword" -dlPath "C:\Temp\"
+#>
+    Param(
+        [Parameter(Mandatory = $true)]$dlNumber,
+        [Parameter(Mandatory = $true)]$dlEXE,
+        [Parameter(Mandatory = $true)]$dlPath,
+        [Parameter(Mandatory = $true)]$CitrixUserName,
+        [Parameter(Mandatory = $true)]$CitrixPassword
+    )
+    #Initialize Session
+    Invoke-WebRequest "https://identity.citrix.com/Utility/STS/Sign-In?ReturnUrl=%2fUtility%2fSTS%2fsaml20%2fpost-binding-response" -SessionVariable websession -UseBasicParsing | Out-Null
+
+    #Set Form
+    $Form = @{
+        "persistent" = "on"
+        "userName"   = $CitrixUserName
+        "password"   = $CitrixPassword
+    }
+
+    #Authenticate
+    Try {
+        Invoke-WebRequest -Uri ("https://identity.citrix.com/Utility/STS/Sign-In?ReturnUrl=%2fUtility%2fSTS%2fsaml20%2fpost-binding-response") -WebSession $websession -Method POST -Body $form -ContentType "application/x-www-form-urlencoded" -UseBasicParsing -ErrorAction Stop | Out-Null
+    }
+    Catch {
+        If ($_.Exception.Response.StatusCode.Value__ -eq 500) {
+            Write-Verbose "500 returned on auth. Ignoring"
+            Write-Verbose $_.Exception.Response
+            Write-Verbose $_.Exception.Message
+        }
+        Else {
+            Throw $_
+        }
+    }
+
+    $dlURL = "https://secureportal.citrix.com/Licensing/Downloads/UnrestrictedDL.aspx?DLID=${dlNumber}&URL=https://downloads.citrix.com/${dlNumber}/${dlEXE}"
+    $Download = Invoke-WebRequest -Uri $dlURL -WebSession $WebSession -UseBasicParsing -Method GET
+    $Webform = @{
+        "chkAccept"            = "on"
+        "clbAccept"            = "Accept"
+        "__VIEWSTATEGENERATOR" = ($Download.InputFields | Where-Object { $_.id -eq "__VIEWSTATEGENERATOR" }).value
+        "__VIEWSTATE"          = ($Download.InputFields | Where-Object { $_.id -eq "__VIEWSTATE" }).value
+        "__EVENTVALIDATION"    = ($Download.InputFields | Where-Object { $_.id -eq "__EVENTVALIDATION" }).value
+    }
+
+    $OutFile = ($dlPath + $dlEXE)
+    #Download
+    Invoke-WebRequest -Uri $dlURL -WebSession $WebSession -Method POST -Body $Webform -ContentType "application/x-www-form-urlencoded" -UseBasicParsing -OutFile $OutFile
+    return $OutFile
+}
 
 If ($appVersion -gt $appInstalledVersion) {
     Set-Location -Path $appScriptDirectory
     If (-Not(Test-Path -Path $appVersion)) {New-Folder -Path $appVersion}
     Set-Location -Path $appVersion
-    Copy-File -Path ".\*" -Destination "$env:SystemDrive\Installs\VDA" -Recurse
-    Set-Location -Path "$env:SystemDrive\Installs\VDA"
 
-    <#
-    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup)) {
-        Write-Log -Message "Downloading $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
+    # Install Windows Media Player feature if missing
+    If ($envOSName -Like "*Windows 10*") {
+        If ((Get-WindowsOptionalFeature –FeatureName "WindowsMediaPlayer" -Online).State -ne "Enabled") {
+            Enable-WindowsOptionalFeature –FeatureName "WindowsMediaPlayer" -All -Online
+        }
+    }
+
+    # Fix VDA install error - https://www.thewindowsclub.com/computer-missing-media-features-icloud-windows-error
+    If (Test-Path -Path "$envProgramFiles\Windows Media Player\wmplayer.exe") {
+        $WindowsMediaPlayerVersion = (Get-FileVersion -File "$envProgramFiles\Windows Media Player\setup_wm.exe" -ProductVersion)
+        If ((Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\WindowsFeatures\WindowsMediaVersion") -and (Get-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\WindowsFeatures\WindowsMediaVersion" -Value "(Default)") -eq "") {
+            Write-Log -Message "Windows Media Player version is empty" -Severity 1 -LogType CMTrace -WriteHost $True
+            Set-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\WindowsFeatures\WindowsMediaVersion" -Name "(Default)" -Value $WindowsMediaPlayerVersion -Type "DWord"
+        }
+    }
+
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup) -or (Get-ChildItem).Length -lt 1024kb) {
+        Write-Log -Message "Citrix credentials for downloading the $appVendor $appName2" -Severity 1 -LogType CMTrace -WriteHost $True
+        $CitrixUserName = Read-Host -Prompt "Please supply your Citrix.com username"
+        $CitrixPassword1 = Read-Host -Prompt "Please supply your Citrix.com password" -AsSecureString
+        $CitrixPassword2 = Read-Host -Prompt "Please supply your Citrix.com password once more" -AsSecureString
+        $CitrixPassword1Temp = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($CitrixPassword1))
+        $CitrixPassword2Temp = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($CitrixPassword2))
+
+        If ($CitrixPassword1Temp -ne $CitrixPassword2Temp) {
+            Write-Log -Message "The supplied Citrix passwords missmatch!" -Severity 3 -LogType CMTrace -WriteHost $True
+            Exit-Script -ExitCode 1
+        }
+
+        Remove-Variable -Name CitrixPassword1Temp, CitrixPassword2Temp
+        $CitrixCredentials = New-Object System.Management.Automation.PSCredential ($CitrixUserName, $CitrixPassword1)
+
+        # Verify Citrix credentials
+        $CitrixUserName = $CitrixCredentials.UserName
+        $CitrixPassword = $CitrixCredentials.GetNetworkCredential().Password
+
+        # Download latest version
+        Write-Log -Message "Downloading $appVendor $appName2 $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Get-CitrixDownload -dlNumber $appDlNumber -dlEXE $appSetup -CitrixUserName $CitrixUserName -CitrixPassword $CitrixPassword -dlPath .\
     }
     Else {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
-    #>
 
+    # Copy $appSetup to C:\Installs\VDA to avoid install issue
+    Copy-File -Path ".\*" -Destination "$env:SystemDrive\Installs\VDA" -Recurse
+    Set-Location -Path "$env:SystemDrive\Installs\VDA"
+
+    # Uninstall previous versions
     Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
     Get-Process -Name $appProcesses | Stop-Process -Force
 
+    # Install latest version
     Write-Log -Message "Installing $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
     Execute-Process -Path .\$appSetup -Parameters $appInstallParameters -WaitForMsiExec -IgnoreExitCodes "3"
 
@@ -125,13 +232,16 @@ If ($appVersion -gt $appInstalledVersion) {
     Execute-Process -Path "$envSystem32Directory\powercfg.exe" -Parameters "/requestsoverride PROCESS picaSessionAgent.exe DISPLAY"
     Execute-Process -Path "$envSystem32Directory\powercfg.exe" -Parameters "/requestsoverride PROCESS GFXMGR.exe DISPLAY"
 
+    # Registry optimizations
+    # Enable EDT MTU Discovery on the VDA - https://docs.citrix.com/en-us/citrix-virtual-apps-desktops/technical-overview/hdx/adaptive-transport.html
+    Set-RegistryKey -Key "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Wds\icawd" -Name "MtuDiscovery" -Type "DWord" -Value "1"
+
     Set-Location -Path $appScriptDirectory
     Remove-Folder -Path "$env:SystemDrive\Installs"
 
     Write-Log -Message "$appVendor $appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 
     Write-Log -Message "$appVendor $appName2" -Text "A reboot required after $appVendor $appName2 $appVersion installation. The computer $envComputerName will reboot in 30 seconds!" -Severity 2 -LogType CMTrace -WriteHost $True
-    Show-DialogBox -Title "$appVendor $appName2" -Text "A reboot required after $appVendor $appName2 $appVersion installation. The computer $envComputerName will reboot in 30 seconds!" -Timeout "10" -Icon "Exclamation"
     Show-InstallationRestartPrompt -Countdownseconds 30 -CountdownNoHideSeconds 30
 }
 Else {
