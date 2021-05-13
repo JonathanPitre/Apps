2cdca8ba3fa5f6eb2a51b3b11e8bcfaa6bb167df
Jonathan Pitre <jakeld@gmail.com>
2021-05-13 00:23:26 -0400

    Change formatting to Allman style Disable update task

--- Microsoft/OneDrive for Business/Install.ps1
+++ Microsoft/OneDrive for Business/Install.ps1
@@ -15,8 +15,11 @@ Write-Verbose -Message "Importing custom modules..." -Verbose
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
 
 # Install custom package providers list
-Foreach ($PackageProvider in $PackageProviders) {
-    If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
+Foreach ($PackageProvider in $PackageProviders)
+{
+    If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue))
+    { Install-PackageProvider -Name $PackageProvider -Force
+    }
 }
 
 # Add the Powershell Gallery as trusted repository
@@ -25,17 +28,23 @@ Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 # Update PowerShellGet
 $InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
 $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
-If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
+If ($PSGetVersion -gt $InstalledPSGetVersion)
+{ Install-PackageProvider -Name PowerShellGet -Force
+}
 
 # Install and import custom modules list
-Foreach ($Module in $Modules) {
-    If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
-    Else {
+Foreach ($Module in $Modules)
+{
+    If (-not(Get-Module -ListAvailable -Name $Module))
+    { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force
+    } Else
+    {
         $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
         $ModuleVersion = (Find-Module -Name $Module).Version
         $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
         $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
-        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion) {
+        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+        {
             Update-Module -Name $Module -Force
             Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
         }
@@ -45,18 +54,27 @@ Foreach ($Module in $Modules) {
 Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
 
 # Get the current script directory
-Function Get-ScriptDirectory {
+Function Get-ScriptDirectory
+{
     Remove-Variable appScriptDirectory
-    Try {
-        If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
-        ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
-        ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
-        Else {
+    Try
+    {
+        If ($psEditor)
+        { Split-Path $psEditor.GetEditorContext().CurrentFile.Path
+        } # Visual Studio Code Host
+        ElseIf ($psISE)
+        { Split-Path $psISE.CurrentFile.FullPath
+        } # Windows PowerShell ISE Host
+        ElseIf ($PSScriptRoot)
+        { $PSScriptRoot
+        } # Windows PowerShell 3.0-5.1
+        Else
+        {
             Write-Host -ForegroundColor Red "Cannot resolve script file's path"
             Exit 1
         }
-    }
-    Catch {
+    } Catch
+    {
         Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
         Exit 2
     }
@@ -88,23 +106,28 @@ $appUninstallString = ((Get-InstalledApplication -Name "$appVendor $appName").Un
 $appUninstallParameters = ((Get-InstalledApplication -Name "$appVendor $appName").UninstallString).TrimStart($appUninstallString)
 ##*===============================================
 
-If ([version]$appVersion -gt [version]$appInstalledVersion) {
+If ([version]$appVersion -gt [version]$appInstalledVersion)
+{
     Set-Location -Path $appScriptDirectory
-    If (-Not(Test-Path -Path $appVersion)) {New-Folder -Path $appVersion}
+    If (-Not(Test-Path -Path $appVersion))
+    {New-Folder -Path $appVersion
+    }
     Set-Location -Path $appVersion
 
     # Download latest setup file(s)
-    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup)) {
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup))
+    {
         Write-Log -Message "Downloading $appVendor $appName $appLongName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
         Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
-    }
-    Else {
+    } Else
+    {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
     # Uninstall previous versions
     Get-Process -Name $appProcesses | Stop-Process -Force
-    If ($IsAppInstalled) {
+    If ($IsAppInstalled)
+    {
         Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
         Execute-Process -Path $appUninstallString -Parameters $appUninstallParameters
     }
@@ -124,7 +147,9 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
     $OneDriveSetup = Get-ItemProperty "HKUDefaultHive:\DefaultHive\Software\Microsoft\Windows\CurrentVersion\Run" | Select-Object -ExpandProperty "OneDriveSetup"
 
     # If Variable returns True, remove the OneDriveSetup Value
-    If ($OneDriveSetup) { Remove-ItemProperty -Path "HKUDefaultHive:\DefaultHive\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDriveSetup" }
+    If ($OneDriveSetup)
+    { Remove-ItemProperty -Path "HKUDefaultHive:\DefaultHive\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDriveSetup"
+    }
 
     # Unload Hive
     Execute-Process -Path CMD.EXE -Parameters "/C REG.EXE UNLOAD HKU\DefaultHive" -Wait -WindowStyle Hidden
@@ -133,18 +158,23 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
     Remove-PSDrive HKUDefaultHive
 
     # Get latest policy definitions
-    If (Test-Path -Path "$appDestination\$appVersion\adm\$appName.admx") {
+    If (Test-Path -Path "$appDestination\$appVersion\adm\$appName.admx")
+    {
         Write-Log -Message "Copying $appVendor $appName $appLongName $appVersion ADMX templates..." -Severity 1 -LogType CMTrace -WriteHost $True
         Copy-File -Path "$appDestination\$appVersion\adm\$appName.admx" -Destination "$appScriptDirectory\PolicyDefinitions" -ContinueOnError $True
         Copy-File -Path "$appDestination\$appVersion\adm\$appName.adml" -Destination "$appScriptDirectory\PolicyDefinitions\en-US" -ContinueOnError $True
         Copy-File -Path "$appDestination\$appVersion\adm\fr\$appName.adml" -Destination "$appScriptDirectory\PolicyDefinitions\fr-FR" -ContinueOnError $True
     }
 
+    # Stop and disable unneeded scheduled tasks
+    Get-ScheduledTask -TaskName "$appName*" | Stop-ScheduledTask
+    Get-ScheduledTask -TaskName "$appName*" | Disable-ScheduledTask
+
     # Go back to the parent folder
     Set-Location ..
 
     Write-Log -Message "$appVendor $appName $appLongName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
-}
-Else {
+} Else
+{
     Write-Log -Message "$appVendor $appName $appLongName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
