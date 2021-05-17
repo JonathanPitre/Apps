4ab11821f4007f39727ca26bd7a4b7361e5d0873
Jonathan Pitre <jakeld@gmail.com>
2021-05-17 16:14:30 -0400

    Add function Get-AdobeCreativeCloud Change formatting to allman style

--- Adobe/Creative Cloud/Install.ps1
+++ Adobe/Creative Cloud/Install.ps1
@@ -7,7 +7,7 @@
 $PackageProviders = @("Nuget")
 
 # Custom modules list
-$Modules = @("PSADT", "Evergreen")
+$Modules = @("PSADT", "Nevergreen")
 
 Write-Verbose -Message "Importing custom modules..." -Verbose
 
@@ -15,7 +15,8 @@ Write-Verbose -Message "Importing custom modules..." -Verbose
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
 
 # Install custom package providers list
-Foreach ($PackageProvider in $PackageProviders) {
+Foreach ($PackageProvider in $PackageProviders)
+{
     If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
 }
 
@@ -28,14 +29,17 @@ $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
 If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
 
 # Install and import custom modules list
-Foreach ($Module in $Modules) {
+Foreach ($Module in $Modules)
+{
     If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
-    Else {
+    Else
+    {
         $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
         $ModuleVersion = (Find-Module -Name $Module).Version
         $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
         $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
-        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion) {
+        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+        {
             Update-Module -Name $Module -Force
             Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
         }
@@ -45,18 +49,22 @@ Foreach ($Module in $Modules) {
 Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
 
 # Get the current script directory
-Function Get-ScriptDirectory {
+Function Get-ScriptDirectory
+{
     Remove-Variable appScriptDirectory
-    Try {
+    Try
+    {
         If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
         ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
         ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
-        Else {
+        Else
+        {
             Write-Host -ForegroundColor Red "Cannot resolve script file's path"
             Exit 1
         }
     }
-    Catch {
+    Catch
+    {
         Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
         Exit 2
     }
@@ -71,6 +79,87 @@ $appScriptDirectory = Get-ScriptDirectory
 
 # Application related
 ##*===============================================
+
+Function Get-AdobeCreativeCloud
+{
+    <#
+    .NOTES
+        Author: Jonathan Pitre
+        Twitter: @PitreJonathan
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding()]
+    Param()
+
+    $VersionDesktop = ((Invoke-WebRequest 'https://helpx.adobe.com/ie/creative-cloud/release-note/cc-release-notes.html' -UseBasicParsing).Content | Select-String -Pattern '((?:\d+\.)+\d+).+mandatory release').Matches.Groups[1].Value
+    $URLDesktop = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v1/wam/download?sapCode=KCCC&productName=Creative%20Cloud&os=win&environment=prod&api_key=CCHomeWeb1'
+
+    $URL64ZipEnterprise = ((Invoke-WebRequest -Uri 'https://helpx.adobe.com/ca/download-install/kb/creative-cloud-desktop-app-download.html' -UseBasicParsing).Links | Where-Object href -Like '*win64*')[0].href
+    $Version64Enterprise = ($URL64ZipEnterprise | Select-String -Pattern 'ACCCx((?:\d+_)+(?:\d+)).zip$').Matches.Groups[1] -replace ("_", ".")
+    $URL64Enterprise = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v1/wam/download?sapCode=KCCC&productName=Creative%20Cloud&os=win&environment=prod&api_key=CCHomeWeb1'
+
+    $URL32Enterprise = ((Invoke-WebRequest -Uri 'https://helpx.adobe.com/ca/download-install/kb/creative-cloud-desktop-app-download.html' -UseBasicParsing).Links | Where-Object href -Like '*win32*')[0].href
+    $Version32Enterprise = ($URL32Enterprise | Select-String -Pattern 'ACCCx((?:\d+_)+(?:\d+)).zip$').Matches.Groups[1] -replace ("_", ".")
+
+    $URLARM64Enterprise = ((Invoke-WebRequest -Uri 'https://helpx.adobe.com/ca/download-install/kb/creative-cloud-desktop-app-download.html' -UseBasicParsing).Links | Where-Object href -Like '*winarm64*')[0].href
+    $VersionARM64Enterprise = ($URLARM64Enterprise | Select-String -Pattern 'ACCCx((?:\d+_)+(?:\d+)).zip$').Matches.Groups[1] -replace ("_", ".")
+
+    if ($VersionDesktop -and $URLDesktop)
+    {
+        [PSCustomObject]@{
+            Version      = $Version64Enterprise
+            Architecture = 'x64'
+            Edition      = 'Desktop'
+            Type         = 'Exe'
+            URI          = $URLDesktop
+        }
+    }
+
+    if ($Version64Enterprise -and $URL64Enterprise)
+    {
+        [PSCustomObject]@{
+            Version      = $Version64Enterprise
+            Architecture = 'x64'
+            Edition      = 'Enterprise'
+            Type         = 'Exe'
+            URI          = $URL64Enterprise
+        }
+    }
+
+    if ($Version64Enterprise -and $URL64ZipEnterprise)
+    {
+        [PSCustomObject]@{
+            Version      = $Version64Enterprise
+            Architecture = 'x64'
+            Edition      = 'Enterprise'
+            Type         = 'Zip'
+            URI          = $URL64ZipEnterprise
+        }
+    }
+
+    if ($Version32Enterprise -and $URL32Enterprise)
+    {
+        [PSCustomObject]@{
+            Version      = $Version32Enterprise
+            Architecture = 'x86'
+            Edition      = 'Enterprise'
+            Type         = 'Zip'
+            URI          = $URL32Enterprise
+        }
+    }
+
+    if ($VersionARM64Enterprise -and $URLARM64Enterprise)
+    {
+        [PSCustomObject]@{
+            Version      = $VersionARM64Enterprise
+            Architecture = 'ARM64'
+            Edition      = 'Enterprise'
+            Type         = 'Zip'
+            URI          = $URLARM64Enterprise
+        }
+    }
+}
+
 $appVendor = "Adobe"
 $appName = "Creative Cloud"
 $appSetup = "Creative_Cloud_Set-Up.exe"
@@ -78,38 +167,39 @@ $appProcesses = @("ccxprocess", "Creative Cloud", "Creative Cloud Helper", "CRWi
     "HDHelper", "AdobeUpdateService", "Adobe Update Helper", "Adobe Desktop Service", "AdobeIPCBroker", "ACToolMain", "AdobeGCClient", "AGMService", "AGSService", "AGCInvokerUtility")
 $appServices = @("AdobeUpdateService", "AGMService", "AGSService")
 $appInstallParameters = "--silent" #--INSTALLLANGUAGE=<ProductInstallLanguage>
-$appURLSetup = "https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v1/wam/download?sapCode=KCCC&productName=Creative%20Cloud&os=win&environment=prod&api_key=CCHomeWeb1"
-$appURLVersion = "https://helpx.adobe.com/ie/creative-cloud/release-note/cc-release-notes.html"
-$webRequest = Invoke-WebRequest -UseBasicParsing -Uri ($appURLVersion) -SessionVariable websession
-$regexAppVersion = "Version \d.\d.\d.\d+.+mandatory release"
-$webVersion = $webRequest.RawContent | Select-String -Pattern $regexAppVersion -AllMatches | ForEach-Object { $_.Matches.Value } | Select-Object -First 1
-$appVersion = ($webVersion).Split(" ")[1].TrimEnd("&nbsp;released")
 # How to use the Creative Cloud Cleaner tool - https://helpx.adobe.com/ca/creative-cloud/kb/cc-cleaner-tool-installation-problems.html
 $appURLCleanerTool = "https://swupmf.adobe.com/webfeed/CleanerTool/win/AdobeCreativeCloudCleanerTool.exe"
 $appCleanerTool = Split-Path -Path $appURLCleanerTool -Leaf
 $appCleanerToolParameters = "--cleanupXML=$appScriptDirectory\cleanup.xml"
+$Nevergreen = Get-AdobeCreativeCloud | Where-Object {$_.Architecture -eq "x64" -and $_.Edition -eq 'Enterprise' -and $_.Type -eq 'Exe'}
+$appVersion = $Nevergreen.Version
+$appURL = $Nevergreen.URI
 $appDestination = "${env:ProgramFiles(x86)}\Adobe\Adobe Creative Cloud\Utils"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName")
 $appInstalledVersion = (Get-InstalledApplication -Name "$appVendor $appName").DisplayVersion
 ##*===============================================
 
-If ([version]$appVersion -gt [version]$appInstalledVersion) {
+If ([version]$appVersion -gt [version]$appInstalledVersion)
+{
     Set-Location -Path $appScriptDirectory
     If (-Not(Test-Path -Path $appVersion)) {New-Folder -Path $appVersion}
     Set-Location -Path $appVersion
 
     # Download latest cleaner tool
-    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appCleanerTool)) {
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appCleanerTool))
+    {
         Write-Log -Message "Downloading $appVendor $appName Cleaner Tool..." -Severity 1 -LogType CMTrace -WriteHost $True
         Invoke-WebRequest -UseBasicParsing -Uri $appURLCleanerTool -OutFile $appCleanerTool
     }
-    Else {
+    Else
+    {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
     # Uninstall previous versions
     Get-Process -Name $appProcesses | Stop-Process -Force
-    If ($IsAppInstalled) {
+    If ($IsAppInstalled)
+    {
         Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
         #Execute-Process -Path .\$appCleanerTool -Parameters "$appCleanerToolParameters"
         Execute-Process -Path "$appDestination\Creative Cloud Uninstaller.exe" -Parameters "-uninstall"
@@ -117,22 +207,26 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
     }
 
     # Download latest file installer
-    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup)) {
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup))
+    {
         Write-Log -Message "Downloading $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLSetup -OutFile $appSetup
+        Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
     }
-    Else {
+    Else
+    {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
     # Install latest version
-    If (Test-Path -Path $appScriptDirectory\$appVersion\$appSetup) {
+    If (Test-Path -Path $appScriptDirectory\$appVersion\$appSetup)
+    {
         Write-Log -Message "Installing $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
         Copy-File -Path .\$appSetup -Destination $envSystemDrive\ -ContinueOnError $True
         Execute-Process -Path $envSystemDrive\$appSetup -Parameters $appInstallParameters
         Get-Process -Name $appProcesses | Stop-Process -Force
     }
-    Else {
+    Else
+    {
         Write-Log -Message "Setup file(s) are missing." -Severity 1 -LogType CMTrace -WriteHost $True
         Exit-Script
     }
@@ -164,6 +258,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
 
     Write-Log -Message "$appVendor $appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 }
-Else {
+Else
+{
     Write-Log -Message "$appVendor $appName $appShortVersion $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
