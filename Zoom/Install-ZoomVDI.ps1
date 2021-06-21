f29e9d38843179799c5e44dd558832ea3c47805e
Jonathan Pitre <jakeld@gmail.com>
2021-06-21 14:39:55 -0400

    Add Zoom VDI install script

---
+++ Zoom/Install-ZoomVDI.ps1
@@ -0,0 +1,199 @@
+# Standalone application install script for VDI environment - (C)2021 Jonathan Pitre & Owen Reynolds, inspired by xenappblog.com
+
+#Requires -Version 5.1
+#Requires -RunAsAdministrator
+
+# Custom package providers list
+$PackageProviders = @("Nuget")
+
+# Custom modules list
+$Modules = @("PSADT", "Nevergreen")
+
+Write-Verbose -Message "Importing custom modules..." -Verbose
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+
+# Install custom package providers list
+Foreach ($PackageProvider in $PackageProviders)
+{
+    If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue))
+    {
+        Install-PackageProvider -Name $PackageProvider -Force
+    }
+}
+
+# Add the Powershell Gallery as trusted repository
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+# Update PowerShellGet
+$InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
+$PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
+If ($PSGetVersion -gt $InstalledPSGetVersion)
+{
+    Install-PackageProvider -Name PowerShellGet -Force
+}
+
+# Install and import custom modules list
+Foreach ($Module in $Modules)
+{
+    If (-not(Get-Module -ListAvailable -Name $Module))
+    {
+        Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force
+    }
+    Else
+    {
+        $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
+        $ModuleVersion = (Find-Module -Name $Module).Version
+        $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
+        $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
+        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+        {
+            Update-Module -Name $Module -Force
+            Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
+        }
+    }
+}
+
+Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
+
+# Get the current script directory
+Function Get-ScriptDirectory
+{
+    Remove-Variable appScriptDirectory
+    Try
+    {
+        If ($psEditor)
+        {
+            Split-Path $psEditor.GetEditorContext().CurrentFile.Path
+        } # Visual Studio Code Host
+        ElseIf ($psISE)
+        {
+            Split-Path $psISE.CurrentFile.FullPath
+        } # Windows PowerShell ISE Host
+        ElseIf ($PSScriptRoot)
+        {
+            $PSScriptRoot
+        } # Windows PowerShell 3.0-5.1
+        Else
+        {
+            Write-Host -ForegroundColor Red "Cannot resolve script file's path"
+            Exit 1
+        }
+    }
+    Catch
+    {
+        Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
+        Exit 2
+    }
+}
+
+# Variables Declaration
+# Generic
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$env:SEE_MASK_NOZONECHECKS = 1
+$appScriptDirectory = Get-ScriptDirectory
+
+# Application related
+##*===============================================
+$appShortName = "Zoom"
+$appName = "Zoom Client for VDI"
+$appProcesses = @("Zoom", "Zoom_launcher", "ZoomOutlookIMPlugin")
+$appInstallParameters = "/QB"
+$Nevergreen = Get-NevergreenApp Zoom | Where-Object {$_.Platform -eq "VDI" }
+$appVersion = $Nevergreen.Version
+$appURL = $Nevergreen.URI
+$appSetup = Split-Path -Path $appURL -Leaf
+$appDestination = "${env:ProgramFiles(x86)}\ZoomVDI\bin"
+$appUninstallerURL = "https://support.zoom.us/hc/en-us/article_attachments/360084068792/CleanZoom.zip"
+$appUninstallerZip = Split-Path -Path $appUninstallerURL -Leaf
+$appUninstallerSetup = "CleanZoom.exe"
+[boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appName")
+$appInstalledVersion = If ($IsAppInstalled) { Get-FileVersion $appDestination\Zoom.exe }
+$appInstalledVersion = $appInstalledVersion.Replace(",", ".").Split(".", 1).Substring(0, $appInstalledVersion.Length - 6)
+##*===============================================
+
+If ([version]$appVersion -gt [version]$appInstalledVersion)
+{
+    Set-Location -Path $appScriptDirectory
+    If (-Not(Test-Path -Path $appVersion))
+    {
+        New-Folder -Path $appVersion
+    }
+    Set-Location -Path $appVersion
+
+    # Download required cleanup tool
+    If (-Not(Test-Path -Path $appScriptDirectory\$appUninstallerSetup))
+    {
+        Write-Log -Message "Downloading $appShortName Cleanup Tool..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appUninstallerURL -OutFile $appScriptDirectory\$appUninstallerZip
+        Expand-Archive -Path $appScriptDirectory\$appUninstallerZip -DestinationPath $appScriptDirectory
+        Remove-File -Path $appScriptDirectory\$appUninstallerZip
+    }
+    Else
+    {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    # Uninstall previous versions
+    Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Get-Process -Name $appProcesses | Stop-Process -Force
+    Remove-MSIApplications -Name "$appShortName" -Parameters $appInstallParameters -ContinueOnError $True
+    Execute-Process -Path $appScriptDirectory\$appUninstallerSetup -Parameters "/silent"
+
+    # Remove user install
+    $ZoomUsers = Get-ChildItem -Path "$($env:SystemDrive)\Users"
+    $ZoomUsers | ForEach-Object {
+        Try
+        {
+            If (Test-Path "$($env:SystemDrive)\Users\$($_.Name)\AppData\Roaming\$appShortName\bin\$appShortName.exe")
+            {
+                Remove-Folder -Path "$($env:SystemDrive)\Users\$($_.Name)\AppData\Roaming\$appShortName" -ContinueOnError $True
+                Remove-Folder -Path "$($env:SystemDrive)\Users\$($_.Name)\AppData\Roaming\$($appShortName)VDI" -ContinueOnError $True
+                Remove-Folder -Path "$($env:SystemDrive)\Users\$($_.Name)\AppData\Roaming\Windows\Start Menu\Programs\$appShortName" -ContinueOnError $True
+                Remove-Folder -Path "$($env:SystemDrive)\Users\$($_.Name)\AppData\Roaming\Windows\Start Menu\Programs\$appShortName VDI" -ContinueOnError $True
+            }
+        }
+        Catch
+        {
+            Out-Null
+        }
+    }
+
+    # Remove registry entries from all user profiles - https://www.reddit.com/r/SCCM/comments/fu3q6f/zoom_uninstall_if_anyone_needs_this_information
+    [scriptblock]$HKCURegistrySettings = {
+        Remove-RegistryKey -Key "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Zoom" -Recurse -ContinueOnError $True -SID $UserProfile.SID
+        Remove-RegistryKey -Key "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ZoomUMX" -Recurse -ContinueOnError $True -SID $UserProfile.SID
+        Remove-RegistryKey -Key "HKCU:\Software\Zoom" -Recurse -ContinueOnError $True -SID $UserProfile.SID
+        Remove-RegistryKey -Key "HKCU:\Software\ZoomUMX" -Recurse -ContinueOnError $True -SID $UserProfile.SID
+        Remove-RegistryKey -Key "HKCU:\Software\Policies\Zoom\Zoom Meetings\VDI" -Recurse -ContinueOnError $True -SID $UserProfile.SID
+    }
+    Invoke-HKCURegistrySettingsForAllUsers -RegistrySettings $HKCURegistrySettings
+
+
+    # Download latest setup file(s)
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup))
+    {
+        Write-Log -Message "Downloading $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
+    }
+    Else
+    {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    # Install latest version
+    Write-Log -Message "Installing $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Execute-MSI -Action Install -Path $appSetup -Parameters $appInstallParameters
+
+
+    # Go back to the parent folder
+    Set-Location ..
+
+    Write-Log -Message "$appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
+}
+Else
+{
+    Write-Log -Message "$appName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
+}
