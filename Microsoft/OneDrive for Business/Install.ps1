0753d0d7f6582af6cb2f9764769bad93a6a92727
Jonathan Pitre <jakeld@gmail.com>
2021-07-13 11:30:27 -0400

    Disable OneDrive update service Improve removal of built-in OneDrive Change formatting to allman style

--- Microsoft/OneDrive for Business/Install.ps1
+++ Microsoft/OneDrive for Business/Install.ps1
@@ -18,7 +18,8 @@ Write-Verbose -Message "Importing custom modules..." -Verbose
 Foreach ($PackageProvider in $PackageProviders)
 {
     If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue))
-    { Install-PackageProvider -Name $PackageProvider -Force
+    {
+        Install-PackageProvider -Name $PackageProvider -Force
     }
 }
 
@@ -29,15 +30,18 @@ Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 $InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
 $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
 If ($PSGetVersion -gt $InstalledPSGetVersion)
-{ Install-PackageProvider -Name PowerShellGet -Force
+{
+    Install-PackageProvider -Name PowerShellGet -Force
 }
 
 # Install and import custom modules list
 Foreach ($Module in $Modules)
 {
     If (-not(Get-Module -ListAvailable -Name $Module))
-    { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force
-    } Else
+    {
+        Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force
+    }
+    Else
     {
         $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
         $ModuleVersion = (Find-Module -Name $Module).Version
@@ -60,20 +64,24 @@ Function Get-ScriptDirectory
     Try
     {
         If ($psEditor)
-        { Split-Path $psEditor.GetEditorContext().CurrentFile.Path
+        {
+            Split-Path $psEditor.GetEditorContext().CurrentFile.Path
         } # Visual Studio Code Host
         ElseIf ($psISE)
-        { Split-Path $psISE.CurrentFile.FullPath
+        {
+            Split-Path $psISE.CurrentFile.FullPath
         } # Windows PowerShell ISE Host
         ElseIf ($PSScriptRoot)
-        { $PSScriptRoot
+        {
+            $PSScriptRoot
         } # Windows PowerShell 3.0-5.1
         Else
         {
             Write-Host -ForegroundColor Red "Cannot resolve script file's path"
             Exit 1
         }
-    } Catch
+    }
+    Catch
     {
         Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
         Exit 2
@@ -92,7 +100,8 @@ $appScriptDirectory = Get-ScriptDirectory
 $appVendor = "Microsoft"
 $appName = "OneDrive"
 $appLongName = "for Business"
-$appProcesses = @("OneDrive")
+$appProcesses = @("OneDrive", "explorer")
+$appServices = @("OneDrive Updater Service")
 $appInstallParameters = "/allusers /silent"
 $Evergreen = (Get-EvergreenApp -Name MicrosoftOneDrive | Where-Object {$_.Architecture -eq "AMD64" -and $_.Ring -eq "Insider" -and $_.Type -eq "exe"}) | `
     Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | Select-Object -First 1
@@ -110,7 +119,8 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
 {
     Set-Location -Path $appScriptDirectory
     If (-Not(Test-Path -Path $appVersion))
-    {New-Folder -Path $appVersion
+    {
+        New-Folder -Path $appVersion
     }
     Set-Location -Path $appVersion
 
@@ -119,7 +129,8 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     {
         Write-Log -Message "Downloading $appVendor $appName $appLongName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
         Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
-    } Else
+    }
+    Else
     {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
@@ -132,30 +143,108 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
         Execute-Process -Path $appUninstallString -Parameters $appUninstallParameters
     }
 
+    # Uninstall built-in OneDrive
+    If (Test-Path "$envWinDir\System32\OneDriveSetup.exe")`
+
+    {
+        Execute-Process -Path "$envWinDir\System32\OneDriveSetup.exe" -Parameters "/uninstall"
+    }
+    If (Test-Path "$envWinDir\SysWOW64\OneDriveSetup.exe")`
+
+    {
+        Execute-Process -Path "$envWinDir\SysWOW64\OneDriveSetup.exe" -Parameters "/uninstall" -UseShellExecute -IgnoreExitCodes "-2147219813"
+
+        # Take Ownsership of OneDriveSetup.exe
+        $ACL = Get-ACL -Path "$envWinDir\SysWOW64\OneDriveSetup.exe"
+        $Group = New-Object System.Security.Principal.NTAccount("$envUserName")
+        $ACL.SetOwner($Group)
+        Set-Acl -Path "$envWinDir\SysWOW64\OneDriveSetup.exe" -AclObject $ACL
+
+        # Assign Full R/W Permissions to $envUserName (Administrator)
+        $Acl = Get-Acl "$envWinDir\SysWOW64\OneDriveSetup.exe"
+        $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("$envUserName", "FullControl", "Allow")
+        $Acl.SetAccessRule($Ar)
+        Set-Acl "$envWinDir\SysWOW64\OneDriveSetup.exe" $Acl
+
+        # Take Ownsership of OneDrive.ico
+        $ACL = Get-ACL -Path "$envWinDir\SysWOW64\OneDriveSetup.exe"
+        $Group = New-Object System.Security.Principal.NTAccount("$envUserName")
+        $ACL.SetOwner($Group)
+        Set-Acl -Path "$envWinDir\SysWOW64\OneDriveSetup.exe" -AclObject $ACL
+
+        # Assign Full R/W Permissions to $envUserName (Administrator)
+        $Acl = Get-Acl "$env:SystemRoot\SysWOW64\OneDrive.ico"
+        $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("$envUserName", "FullControl", "Allow")
+        $Acl.SetAccessRule($Ar)
+        Set-Acl "$env:SystemRoot\SysWOW64\OneDrive.ico" $Acl
+
+        Remove-File -Path "$envWinDir\SysWOW64\OneDriveSetup.exe"
+        Remove-File -Path "$envWinDir\SysWOW64\OneDrive.ico"
+    }
+    Remove-RegistryKey -Key "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive"
+
+    # Remove left over files
+    Remove-File -Path "$envWinDir\ServiceProfiles\LocalService\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -ContinueOnError $True
+    Remove-File -Path "$envWinDir\ServiceProfiles\NetworkService\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -ContinueOnError $True
+    Remove-Folder -Path "$envProgramData\Microsoft\OneDrive" -ContinueOnError $True
+    Remove-Folder -Path "$envSystemDrive\OneDriveTemp" -ContinueOnError $True
+
+    # Remove user install
+    $OneDriveUsers = Get-ChildItem -Path "$($envSystemDrive)\Users"
+    $OneDriveUsers | ForEach-Object {
+        Try
+        {
+            If (Test-Path "$($envSystemDrive)\Users\$($_.Name)\AppData\Local\Microsoft\OneDrive.exe")
+            {
+                Remove-Folder -Path "$($envSystemDrive)\Users\$($_.Name)\AppData\Local\Microsoft\OneDrive" -ContinueOnError $True
+                Remove-Folder -Path "$($envSystemDrive)\Users\$($_.Name)\OneDrive" -ContinueOnError $True
+                Remove-File -Path "$($envSystemDrive)\Users\$($_.Name)\AppData\Roaming\$appVendor\Windows\Start Menu\Programs\OneDrive.lnk" -ContinueOnError $True
+            }
+        }
+        Catch
+        {
+            Out-Null
+        }
+    }
+
+    # Remove remaining OneDrive entries WinSxS folders
+    #ForEach ($Item in Get-ChildItem -Path "$envWinDir\WinSxS\*onedrive*")
+    #{
+    #    Execute-Process -Path "$envWinDir\System32\takeown.exe" -Parameters "/F $Item.FullName /R /A"
+    #    [void](Grant-FolderOwnership -Path $Item.FullName)
+    #    Remove-Folder -Path $Item.FullName -ContinueOnError $True
+    #}
+
+    # Removes OneDrive from File Explorer
+    Remove-RegistryKey -Key "HKLM:\SOFTWARE\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse
+    Remove-RegistryKey -Key "HKLM:\SOFTWARE\Wow6432Node\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse
+
     Write-Log -Message "Installing $appVendor $appName $appLongName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
     Execute-Process -Path .\$appSetup -Parameters $appInstallParameters
 
     Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
 
-    # Create PSDrive for HKU - https://byteben.com/bb/installing-the-onedrive-sync-client-in-per-machine-mode-during-your-task-sequence-for-a-lightening-fast-first-logon-experience
-    New-PSDrive -PSProvider Registry -Name HKUDefaultHive -Root HKEY_USERS
+    # Load the Default User registry hive
+    Execute-Process -Path "$envWinDir\System32\reg.exe" -Parameters "LOAD HKLM\DefaultUser $envSystemDrive\Users\Default\NTUSER.DAT" -WindowStyle Hidden
 
-    # Load Default User Hive
-    Execute-Process -Path CMD.EXE -Parameters "/C REG.EXE LOAD HKU\DefaultHive C:\Users\Default\NTUSER.DAT" -WindowStyle Hidden
+    # Set OneDriveSetup variable
+    $OneDriveSetup = Get-RegistryKey -Key "HKLM:\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Run" -Value "OneDriveSetup"
 
-    # Set OneDriveSetup Variable
-    $OneDriveSetup = Get-ItemProperty "HKUDefaultHive:\DefaultHive\Software\Microsoft\Windows\CurrentVersion\Run" | Select-Object -ExpandProperty "OneDriveSetup"
-
-    # If Variable returns True, remove the OneDriveSetup Value
+    # Remove the built-in OneDrive setup from running on new user profile
+    # https://byteben.com/bb/installing-the-onedrive-sync-client-in-per-machine-mode-during-your-task-sequence-for-a-lightening-fast-first-logon-experience
     If ($OneDriveSetup)
-    { Remove-ItemProperty -Path "HKUDefaultHive:\DefaultHive\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDriveSetup"
+    {
+        Remove-RegistryKey -Key "HKLM:\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDriveSetup"
     }
 
-    # Unload Hive
-    Execute-Process -Path CMD.EXE -Parameters "/C REG.EXE UNLOAD HKU\DefaultHive" -Wait -WindowStyle Hidden
+    # Unload the Default User registry hive
+    Execute-Process -Path "$envWinDir\System32\reg.exe" -Parameters "UNLOAD HKLM\DefaultUser" -WindowStyle Hidden
 
-    # Remove PSDrive HKUDefaultHive
-    Remove-PSDrive HKUDefaultHive
+    # Cleanup temp files
+    Remove-Item -Path "$envSystemDrive\Users\Default\*.LOG1" -Force
+    Remove-Item -Path "$envSystemDrive\Users\Default\*.LOG2" -Force
+    Remove-Item -Path "$envSystemDrive\Users\Default\*.blf" -Force
+    Remove-Item -Path "$envSystemDrive\Users\Default\*.regtrans-ms" -Force
 
     # Get latest policy definitions
     If (Test-Path -Path "$appDestination\$appVersion\adm\$appName.admx")
@@ -170,11 +259,18 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     Get-ScheduledTask -TaskName "$appName*" | Stop-ScheduledTask
     Get-ScheduledTask -TaskName "$appName*" | Disable-ScheduledTask
 
+    # Stop and disable unneeded services
+    Stop-ServiceAndDependencies -Name $appServices[0]
+    Set-ServiceStartMode -Name $appServices[0] -StartMode "Disabled"
+
+    Update-GroupPolicy
+
     # Go back to the parent folder
     Set-Location ..
 
     Write-Log -Message "$appVendor $appName $appLongName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
-} Else
+}
+Else
 {
     Write-Log -Message "$appVendor $appName $appLongName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
