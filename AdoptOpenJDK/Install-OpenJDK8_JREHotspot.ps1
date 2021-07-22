0c21659301268e4ecf4c90d8f7b19ba44cafcc20
Jonathan Pitre <jakeld@gmail.com>
2021-07-22 12:30:01 -0400

    Replace web scarpping code with Nevergreen module Rename Install script

--- AdoptOpenJDK/Install-OpenJDK8_JREHotspot.ps1
+++ AdoptOpenJDK/Install-OpenJDK8_JREHotspot.ps1
@@ -7,7 +7,7 @@
 $PackageProviders = @("Nuget")
 
 # Custom modules list
-$Modules = @("PSADT", "Evergreen")
+$Modules = @("PSADT", "Nevergreen")
 
 Write-Verbose -Message "Importing custom modules..." -Verbose
 
@@ -15,7 +15,8 @@ Write-Verbose -Message "Importing custom modules..." -Verbose
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
 
 # Install custom package providers list
-Foreach ($PackageProvider in $PackageProviders) {
+Foreach ($PackageProvider in $PackageProviders)
+{
     If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
 }
 
@@ -28,14 +29,17 @@ $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
 If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
 
 # Install and import custom modules list
-Foreach ($Module in $Modules) {
+Foreach ($Module in $Modules)
+{
     If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
-    Else {
+    Else
+    {
         $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
         $ModuleVersion = (Find-Module -Name $Module).Version
         $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
         $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
-        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion) {
+        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+        {
             Update-Module -Name $Module -Force
             Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
         }
@@ -45,18 +49,22 @@ Foreach ($Module in $Modules) {
 Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
 
 # Get the current script directory
-Function Get-ScriptDirectory {
+Function Get-ScriptDirectory
+{
     Remove-Variable appScriptDirectory
-    Try {
+    Try
+    {
         If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
         ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
         ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
-        Else {
+        Else
+        {
             Write-Host -ForegroundColor Red "Cannot resolve script file's path"
             Exit 1
         }
     }
-    Catch {
+    Catch
+    {
         Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
         Exit 2
     }
@@ -74,22 +82,23 @@ $appScriptDirectory = Get-ScriptDirectory
 $appVendor = "AdoptOpenJDK"
 $appName = "JRE"
 $appMajorVersion = "8"
+$appPlatform = "HotSpot Normal JRE"
+$appArchitecture = "x64"
 $appProcesses = @("java", "javaw", "javaws")
 $appInstallParameters = "/QB"
 $appAddParameters = "ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome,FeatureIcedTeaWeb,FeatureJNLPFileRunWith"
-$webResponse = Invoke-WebRequest -UseBasicParsing -Uri ("https://github.com/AdoptOpenJDK/openjdk$appMajorVersion-binaries/releases") -SessionVariable websession
-$regex = "\/AdoptOpenJDK\/openjdk$appMajorVersion-binaries\/releases\/download\/jdk\du\d{3}-b\d+.+\/OpenJDK$($appMajorVersion)U-jre_x64_windows_hotspot_$($appMajorVersion)u\d{3}b\d+\.msi"
-$appURL = "https://github.com" + ($webResponse.RawContent | Select-String -Pattern $regex -AllMatches | ForEach-Object { $_.Matches.Value } | Select-Object -First 1)
-$appSetup = $appURL.Split("/")[8]
-[string]$webVersion = [regex]::matches($appSetup, "\du\d{3}b\d+")
-$appVersion = $webVersion.Replace("u", ".0.").Replace("b", ".")
+$Nevergreen = Get-NevergreenApp AdoptOpenJDK | Where-Object {$_.Name -like "$appVendor$appMajorVersion" -and $_.Platform -eq $appPlatform -and $_.Architecture -eq $appArchitecture -and $_.Type -eq "Msi"}
+$appVersion = $Nevergreen.Version
+$appURL = $Nevergreen.URI
+$appSetup = Split-Path -Path $appURL -Leaf
 $appMinorVersion = $appVersion.Split(".")[2].Substring(0, 3)
 $appDestination = "$env:ProgramFiles\$appVendor\jre-$appMajorVersion.0.$appMinorVersion.1-hotspot\bin"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName")
 $appInstalledVersion = (Get-InstalledApplication -Name "$appVendor $appName").DisplayVersion
 ##*================================================
 
-If ([version]$appVersion -gt [version]$appInstalledVersion) {
+If ([version]$appVersion -gt [version]$appInstalledVersion)
+{
     Set-Location -Path $appScriptDirectory
     If (-Not(Test-Path -Path $appVersion)) {New-Folder -Path $appVersion}
     Set-Location -Path $appVersion
@@ -98,16 +107,19 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
     Get-Process -Name $appProcesses | Stop-Process -Force
     # Uninstall java
     Remove-MSIApplications -Name "Java"
-    If ($IsAppInstalled) {
+    If ($IsAppInstalled)
+    {
         Remove-MSIApplications -Name "$appVendor $appName"
     }
 
     # Download latest setup file(s)
-    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup)) {
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup))
+    {
         Write-Log -Message "Downloading $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
         Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
     }
-    Else {
+    Else
+    {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
@@ -144,6 +156,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
 
     Write-Log -Message "$appVendor $appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 }
-Else {
+Else
+{
     Write-Log -Message "$appVendor $appName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
