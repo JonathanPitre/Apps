f5fc5e6a3a717e1d4c74d29a0fac1ad3ee9b243b
Jonathan Pitre <jakeld@gmail.com>
2021-09-15 09:43:41 -0400

    Replace download with Nevergreen Replace ini file with Set-IniValuue

--- Notepad3/Install.ps1
+++ Notepad3/Install.ps1
@@ -7,7 +7,7 @@
 $PackageProviders = @("Nuget")
 
 # Custom modules list
-$Modules = @("PSADT", "Evergreen")
+$Modules = @("PSADT", "Nevergreen")
 
 Write-Verbose -Message "Importing custom modules..." -Verbose
 
@@ -15,7 +15,8 @@ Write-Verbose -Message "Importing custom modules..." -Verbose
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
 
 # Install custom package providers list
-Foreach ($PackageProvider in $PackageProviders) {
+Foreach ($PackageProvider in $PackageProviders)
+{
     If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
 }
 
@@ -28,14 +29,17 @@ $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
 If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
 
 # Install and import custom modules list
-Foreach ($Module in $Modules) {
+Foreach ($Module in $Modules)
+{
     If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
-    Else {
+    Else
+    {
         $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
         $ModuleVersion = (Find-Module -Name $Module).Version
         $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
         $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
-        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion) {
+        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+        {
             Update-Module -Name $Module -Force
             Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
         }
@@ -45,18 +49,22 @@ Foreach ($Module in $Modules) {
 Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
 
 # Get the current script directory
-Function Get-ScriptDirectory {
+Function Get-ScriptDirectory
+{
     Remove-Variable appScriptDirectory
-    Try {
+    Try
+    {
         If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
         ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
         ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
-        Else {
+        Else
+        {
             Write-Host -ForegroundColor Red "Cannot resolve script file's path"
             Exit 1
         }
     }
-    Catch {
+    Catch
+    {
         Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
         Exit 2
     }
@@ -75,31 +83,33 @@ $appVendor = "Rizonesoft"
 $appName = "Notepad3"
 $appProcesses = @("Notepad3", "minipath", "notepad")
 $appInstallParameters = "/ALLUSERS /CLOSEAPPLICATIONS /LOADINF=`"$appScriptDirectory\Notepad3.inf`" /SILENT /LOG=`"$appScriptDirectory\$appName.log`""
+$appArchitecture = "x64"
 $appPreferredLanguage = "fr-FR"
-$webRequest = Invoke-WebRequest -UseBasicParsing -Uri ("https://www.rizonesoft.com/downloads/notepad3") -SessionVariable websession
-$regexURL = "https\:\/\/www\.rizonesoft\.com\/download\/\d.+/"
-$regexZip = "Notepad3_\d.\d{2}.\d{3}.\d_Setup.zip"
-$appURL = $webRequest.RawContent | Select-String -Pattern $regexURL -AllMatches | ForEach-Object { $_.Matches.Value } | Select-Object -First 1
-$appZip = $webRequest.RawContent | Select-String -Pattern $regexZip -AllMatches | ForEach-Object { $_.Matches.Value } | Select-Object -First 1
-$appVersion = $appZip.Split("_")[1]
-$appSetup = "Notepad3_$($appVersion)_Setup.exe"
+$Nevergreen = Get-NevergreenApp -Name $appVendor$appName | Where-Object { $_.Architecture -eq $appArchitecture}
+$appVersion = $Nevergreen.Version
+$appURL = $Nevergreen.URI
+$appZip = "$($appName)_$($appVersion)_Setup.zip"
+$appSetup = "$($appName)_$($appVersion)_Setup.exe"
 $appDestination = "$env:ProgramFiles\Notepad3"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appName")
 $appInstalledVersion = (Get-InstalledApplication -Name "$appName").DisplayVersion
 ##*===============================================
 
-If ([version]$appVersion -gt [version]$appInstalledVersion) {
+If ([version]$appVersion -gt [version]$appInstalledVersion)
+{
     Set-Location -Path $appScriptDirectory
     If (-Not(Test-Path -Path $appVersion)) { New-Folder -Path $appVersion }
     Set-Location -Path $appVersion
 
-    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup)) {
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup))
+    {
         Write-Log -Message "Downloading $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
         Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appZip
         Expand-Archive -Path $appZip -DestinationPath $appScriptDirectory\$appVersion
         Remove-File -Path $appZip
     }
-    Else {
+    Else
+    {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
@@ -109,20 +119,22 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
     Write-Log -Message "Installing $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
     Execute-Process -Path ".\$appSetup" -Parameters $appInstallParameters
     Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
-    <#
-        If (Test-Path -Path $appDestination\Notepad3.ini) {
-        Set-IniValue -FilePath "$appDestination\Notepad3.ini" -Section "Settings" -Key "SettingsVersion" -Value "4"
+    If (Test-Path -Path $appDestination\Notepad3.ini) {
+        Set-IniValue -FilePath "$appDestination\Notepad3.ini" -Section "Notepad3" -Key "Notepad3.ini" -Value "%APPDATA%\Rizonesoft\Notepad3\Notepad3.ini"
+        Set-IniValue -FilePath "$appDestination\Notepad3.ini" -Section "Settings" -Key "SettingsVersion" -Value "5"
+        Set-IniValue -FilePath "$appDestination\Notepad3.ini" -Section "Settings" -Key "AutoCompleteWords" -Value "true"
         Set-IniValue -FilePath "$appDestination\Notepad3.ini" -Section "Settings2" -Key "PreferredLanguageLocaleName" -Value "$appPreferredLanguage"
         Set-IniValue -FilePath "$appDestination\Notepad3.ini" -Section "Settings2" -Key "ReuseWindow" -Value "true"
+        Set-IniValue -FilePath "$appDestination\Notepad3.ini" -Section "Settings2" -Key "SingleFileInstance" -Value "1"
     }
-    #>
-    Copy-File -Path "$appScriptDirectory\$appname.ini" -Destination "$envProgramFiles\$appName"
+    #Copy-File -Path "$appScriptDirectory\$appname.ini" -Destination "$envProgramFiles\$appName"
 
     # Go back to the parent folder
     Set-Location ..
 
     Write-Log -Message "$appVendor $appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 }
-Else {
+Else
+{
     Write-Log -Message "$appVendor $appName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
