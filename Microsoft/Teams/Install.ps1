5b677b57651d03021e3421b4d63b7d722f096991
Jonathan Pitre <jakeld@gmail.com>
2021-10-08 14:00:03 -0400

    Improve cleanup

--- Microsoft/Teams/Install.ps1
+++ Microsoft/Teams/Install.ps1
@@ -114,6 +114,123 @@ $appSetup = Split-Path -Path $appURL -Leaf
 $appDestination = "${env:ProgramFiles(x86)}\Microsoft\Teams\current"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName")
 $appInstalledVersion = If ($IsAppInstalled) { Get-FileVersion $appDestination\Teams.exe }
+
+Function Search-Registry
+{
+    <#
+.SYNOPSIS
+Searches registry key names, value names, and value data (limited).
+.DESCRIPTION
+This function can search registry key names, value names, and value data (in a limited fashion). It outputs custom objects that contain the key and the first match type (KeyName, ValueName, or ValueData).
+.EXAMPLE
+Search-Registry -Path HKLM:\SYSTEM\CurrentControlSet\Services\* -SearchRegex "svchost" -ValueData
+.EXAMPLE
+Search-Registry -Path HKLM:\SOFTWARE\Microsoft -Recurse -ValueNameRegex "ValueName1|ValueName2" -ValueDataRegex "ValueData" -KeyNameRegex "KeyNameToFind1|KeyNameToFind2"
+.LINK
+https://github.com/asheroto/Search-Registry
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0, ValueFromPipelineByPropertyName)]
+        [Alias("PsPath")]
+        # Registry path to search
+        [string[]] $Path,
+        # Specifies whether or not all subkeys should also be searched
+        [switch] $Recurse,
+        [Parameter(ParameterSetName = "SingleSearchString", Mandatory)]
+        # A regular expression that will be checked against key names, value names, and value data (depending on the specified switches)
+        [string] $SearchRegex,
+        [Parameter(ParameterSetName = "SingleSearchString")]
+        # When the -SearchRegex parameter is used, this switch means that key names will be tested (if none of the three switches are used, keys will be tested)
+        [switch] $KeyName,
+        [Parameter(ParameterSetName = "SingleSearchString")]
+        # When the -SearchRegex parameter is used, this switch means that the value names will be tested (if none of the three switches are used, value names will be tested)
+        [switch] $ValueName,
+        [Parameter(ParameterSetName = "SingleSearchString")]
+        # When the -SearchRegex parameter is used, this switch means that the value data will be tested (if none of the three switches are used, value data will be tested)
+        [switch] $ValueData,
+        [Parameter(ParameterSetName = "MultipleSearchStrings")]
+        # Specifies a regex that will be checked against key names only
+        [string] $KeyNameRegex,
+        [Parameter(ParameterSetName = "MultipleSearchStrings")]
+        # Specifies a regex that will be checked against value names only
+        [string] $ValueNameRegex,
+        [Parameter(ParameterSetName = "MultipleSearchStrings")]
+        # Specifies a regex that will be checked against value data only
+        [string] $ValueDataRegex
+    )
+
+    begin
+    {
+        switch ($PSCmdlet.ParameterSetName)
+        {
+            SingleSearchString
+            {
+                $NoSwitchesSpecified = -not ($PSBoundParameters.ContainsKey("KeyName") -or $PSBoundParameters.ContainsKey("ValueName") -or $PSBoundParameters.ContainsKey("ValueData"))
+                if ($KeyName -or $NoSwitchesSpecified) { $KeyNameRegex = $SearchRegex }
+                if ($ValueName -or $NoSwitchesSpecified) { $ValueNameRegex = $SearchRegex }
+                if ($ValueData -or $NoSwitchesSpecified) { $ValueDataRegex = $SearchRegex }
+            }
+            MultipleSearchStrings
+            {
+                # No extra work needed
+            }
+        }
+    }
+
+    process
+    {
+        foreach ($CurrentPath in $Path)
+        {
+            Get-ChildItem $CurrentPath -Recurse:$Recurse |
+                ForEach-Object {
+                $Key = $_
+
+                if ($KeyNameRegex)
+                {
+                    Write-Verbose ("{0}: Checking KeyNamesRegex" -f $Key.Name)
+
+                    if ($Key.PSChildName -match $KeyNameRegex)
+                    {
+                        Write-Verbose "  -> Match found!"
+                        return [PSCustomObject] @{
+                            Key    = $Key
+                            Reason = "KeyName"
+                        }
+                    }
+                }
+
+                if ($ValueNameRegex)
+                {
+                    Write-Verbose ("{0}: Checking ValueNamesRegex" -f $Key.Name)
+
+                    if ($Key.GetValueNames() -match $ValueNameRegex)
+                    {
+                        Write-Verbose "  -> Match found!"
+                        return [PSCustomObject] @{
+                            Key    = $Key
+                            Reason = "ValueName"
+                        }
+                    }
+                }
+
+                if ($ValueDataRegex)
+                {
+                    Write-Verbose ("{0}: Checking ValueDataRegex" -f $Key.Name)
+
+                    if (($Key.GetValueNames() | ForEach-Object { $Key.GetValue($_) }) -match $ValueDataRegex)
+                    {
+                        Write-Verbose "  -> Match!"
+                        return [PSCustomObject] @{
+                            Key    = $Key
+                            Reason = "ValueData"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 ##*===============================================
 
 If ([version]$appVersion -gt [version]$appInstalledVersion)
@@ -133,9 +250,25 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     Remove-MSIApplications -Name "$appVendor $appName" -Parameters $appInstallParameters -ContinueOnError $True
     Remove-MSIApplications -Name "$appName Machine-Wide Installer" -Parameters $appInstallParameters -ContinueOnError $True
 
-    # Delete left over reg keys
+    # Delete left over folders and reg keys
+    Remove-Folder -Path "$envProgramFilesX86\Teams Installer" -ContinueOnError $True
     Remove-RegistryKey -Key "HKCR:\CLSID\{00425F68-FFC1-445F-8EDF-EF78B84BA1C7}" -Recurse -ContinueOnError $True
     Remove-RegistryKey -Key "HKCR:\WOW6432Node\CLSID\{00425F68-FFC1-445F-8EDF-EF78B84BA1C7}" -Recurse -ContinueOnError $True
+    Remove-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\Folders" -Name "C:\Program Files (x86)\Teams Installer\"
+    $RegKey = Search-Registry -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" -Recurse -ValueDataRegex ".*Teams.*Installer.*"  | Select-Object -ExpandProperty Key
+    Remove-RegistryKey -Key $RegKey
+    $RegKeys = Search-Registry -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Components" -Recurse -ValueDataRegex ".*Teams.*Installer.*" | Select-Object -ExpandProperty Key
+    foreach ($RegKey in $RegKeys)
+    {
+        try
+        {
+            Remove-RegistryKey -Key $RegKey
+        }
+        catch
+        {
+        }
+    }
+
 
     # Remove user install
     $TeamsUsers = Get-ChildItem -Path "$($env:SystemDrive)\Users"
