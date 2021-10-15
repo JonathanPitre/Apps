88410b8a7713a8be78e69671a74582ae36bff71b
Jonathan Pitre <jakeld@gmail.com>
2021-10-15 19:38:43 -0400

    Add missing var $appVersion

--- Citrix/Virtual Delivery Agent/Install-WorkstationPVS-LTSR.ps1
+++ Citrix/Virtual Delivery Agent/Install-WorkstationPVS-LTSR.ps1
@@ -88,6 +88,7 @@ $appServices = @("CitrixTelemetryService")
 # https://docs.citrix.com/en-us/citrix-virtual-apps-desktops/install-configure/install-vdas-sccm.html
 $appInstallParameters = '/noreboot /quiet /enable_remote_assistance /disableexperiencemetrics /virtualmachine /optimize /noresume /enable_real_time_transport /nodesktopexperience /enable_hdx_ports /enable_hdx_udp_ports /components vda /masterpvsimage /includeadditional "Citrix User Profile Manager","Citrix User Profile Manager WMI Plugin" /exclude "User personalization layer","AppDisks VDA Plug-in","Citrix Files for Outlook","Citrix Files for Windows","Citrix Supportability Tools","Citrix Personalization for App-V - VDA","Machine Identity Service","Personal vDisk"'
 $Evergreen = Get-EvergreenApp -Name CitrixVirtualAppsDesktopsFeed | Where-Object {$_.Title -like "Citrix Virtual Apps and Desktops 7 * LTSR,*"} | Sort-Object Version -Descending | Select-Object -First 1
+$appVersion = $Evergreen.Version
 $appSetup = "VDAWorkstationSetup_$appVersion.exe"
 $appDlNumber = "19427"
 $appDestination = "$env:ProgramFiles\$appVendor\Virtual Delivery Agent"
