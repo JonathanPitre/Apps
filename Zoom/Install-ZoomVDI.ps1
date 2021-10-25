ced3cd77c727e0fc0d06002796b73d8e59fc8b43
Jonathan Pitre <jakeld@gmail.com>
2021-10-25 11:12:15 -0400

    Add automatic download of policy definitions New Get-Download and Get-FileZie function

--- Zoom/Install-ZoomVDI.ps1
+++ Zoom/Install-ZoomVDI.ps1
@@ -58,23 +58,15 @@ Foreach ($Module in $Modules)
 Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
 
 # Get the current script directory
+# Get the current script directory
 Function Get-ScriptDirectory
 {
     Remove-Variable appScriptDirectory
     Try
     {
-        If ($psEditor)
-        {
-            Split-Path $psEditor.GetEditorContext().CurrentFile.Path
-        } # Visual Studio Code Host
-        ElseIf ($psISE)
-        {
-            Split-Path $psISE.CurrentFile.FullPath
-        } # Windows PowerShell ISE Host
-        ElseIf ($PSScriptRoot)
-        {
-            $PSScriptRoot
-        } # Windows PowerShell 3.0-5.1
+        If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
+        ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
+        ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
         Else
         {
             Write-Host -ForegroundColor Red "Cannot resolve script file's path"
@@ -87,6 +79,39 @@ Function Get-ScriptDirectory
         Exit 2
     }
 }
+Filter Get-FileSize
+{
+    "{0:N2} {1}" -f $(
+        If ($_ -lt 1kb) { $_, 'Bytes' }
+        ElseIf ($_ -lt 1mb) { ($_ / 1kb), 'KB' }
+        ElseIf ($_ -lt 1gb) { ($_ / 1mb), 'MB' }
+        ElseIf ($_ -lt 1tb) { ($_ / 1gb), 'GB' }
+        ElseIf ($_ -lt 1pb) { ($_ / 1tb), 'TB' }
+        Else { ($_ / 1pb), 'PB' }
+    )
+}
+Function Get-Download
+{
+    Param (
+        [Parameter(Mandatory = $true)]
+        $Url,
+        $Destination = $appScriptDirectory,
+        $FileName,
+        [switch]$IncludeStats
+    )
+    $destinationPath = Join-Path -Path $Destination -ChildPath $FileName
+    $start = Get-Date
+    Invoke-WebRequest -UseBasicParsing -Uri $Url -DisableKeepAlive -OutFile $destinationPath
+    $timeElapsed = ((Get-Date) - $start).ToString('hh\:mm\:ss')
+    $fileSize = (Get-Item -Path $destinationPath).Length | Get-FileSize
+    If ($IncludeStats.IsPresent)
+    {
+        $downloadStats = [PSCustomObject]@{FileSize = $fileSize; Time = $timeElapsed}
+        Write-Information -MessageData $downloadStats
+    }
+    Get-Item -Path $destinationPath| Unblock-File
+}
+
 
 # Variables Declaration
 # Generic
@@ -97,6 +122,28 @@ $appScriptDirectory = Get-ScriptDirectory
 
 # Application related
 ##*===============================================
+
+Function Get-ZoomAdmx
+{
+    Try
+    {
+        $sourceUrl = "https://support.zoom.us/hc/en-us/articles/360039100051"
+        # Grab content
+        $web = Invoke-WebRequest -Uri $sourceUrl -UseBasicParsing
+        # Find ADMX download
+        $admxUrl = (($web.links | Where-Object {$_.href -like "*msi-templates*.zip"})[-1]).href
+        # Grab version
+        $admxVersion = ($admxUrl.Split("/")[-1] | Select-String -Pattern "(\d+(\.\d+){1,4})" -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }).ToString()
+
+        # Return object
+        return @{ Version = $admxVersion; URI = $admxUrl }
+    }
+    Catch
+    {
+        Throw $_
+    }
+}
+
 $appShortName = "Zoom"
 $appName = "Zoom Client for VDI"
 $appProcesses = @("Zoom", "Zoom_launcher", "ZoomOutlookIMPlugin")
@@ -111,6 +158,9 @@ $appDestination = "${env:ProgramFiles(x86)}\ZoomVDI\bin"
 $appUninstallerURL = "https://support.zoom.us/hc/en-us/article_attachments/360084068792/CleanZoom.zip"
 $appUninstallerZip = Split-Path -Path $appUninstallerURL -Leaf
 $appUninstallerSetup = "CleanZoom.exe"
+$appAdmxVersion = (Get-ZoomAdmx).Version
+$appAdmxUrl = (Get-ZoomAdmx).URI
+$appAdmx = Split-Path -Path $appAdmxUrl -Leaf
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appName")
 $appInstalledVersion = If ($IsAppInstalled) { Get-FileVersion $appDestination\Zoom.exe }
 $appInstalledVersion = $appInstalledVersion.Replace(",", ".").Split(".", 1).Substring(0, $appInstalledVersion.Length - 6)
@@ -129,7 +179,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     If (-Not(Test-Path -Path $appScriptDirectory\$appUninstallerSetup))
     {
         Write-Log -Message "Downloading $appShortName Cleanup Tool..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appUninstallerURL -OutFile $appScriptDirectory\$appUninstallerZip
+        Get-Download -Url $appUninstallerURL -Destination $appScriptDirectory -FileName $appUninstallerZip -IncludeStats
         Expand-Archive -Path $appScriptDirectory\$appUninstallerZip -DestinationPath $appScriptDirectory
         Remove-File -Path $appScriptDirectory\$appUninstallerZip
     }
@@ -173,12 +223,23 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     }
     Invoke-HKCURegistrySettingsForAllUsers -RegistrySettings $HKCURegistrySettings
 
+    # Download latest policy definitions
+    Write-Log -Message "Downloading $appShortName ADMX templates $appAdmxVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Get-Download -Url $appAdmxUrl -Destination $appScriptDirectory $appAdmx -IncludeStats
+    Expand-Archive -Path "$appScriptDirectory\$appAdmx" -DestinationPath "$appScriptDirectory\Temp" -Force
+    Remove-File -Path "$appScriptDirectory\$appAdmx"
+    If (-Not(Test-Path -Path "$appScriptDirectory\ADMX")) { New-Folder -Path "$appScriptDirectory\ADMX" }
+    Move-Item -Path "$appScriptDirectory\Temp\*\*.admx" -Destination "$appScriptDirectory\ADMX" -Force
+    If (-Not(Test-Path -Path "$appScriptDirectory\ADMX\en-US")) { New-Folder -Path "$appScriptDirectory\ADMX\en-US" }
+    Move-Item -Path "$appScriptDirectory\Temp\*\en-US\*.adml" -Destination "$appScriptDirectory\ADMX\en-US" -Force
+    Remove-Folder -Path "$appScriptDirectory\Temp"
+    Write-Log -Message "$appShortName ADMX templates $appAdmxVersion were downloaded successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 
     # Download latest setup file(s)
     If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup))
     {
         Write-Log -Message "Downloading $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
+        Get-Download -Url $appUrl -Destination $appScriptDirectory\$appVersion -FileName $appSetup -IncludeStats
     }
     Else
     {
