310567123f5f65191ba5e04c122989bdd008aec0
Jonathan Pitre <jakeld@gmail.com>
2021-11-12 14:37:31 -0500

    Add automatic download of setup Improve logging Fix error on service stop

--- Adobe/Acrobat DC/Install.ps1
+++ Adobe/Acrobat DC/Install.ps1
@@ -82,65 +82,57 @@ $appScriptDirectory = Get-ScriptDirectory
 $appVendor = "Adobe"
 $appName = "Acrobat"
 $appShortVersion = "DC"
-$appProcesses = @("Acrobat", "AcroBroker", "acrobat_sl", "AcroCEF", "acrodist", "acrotray", "AcroRd32", "Acrobat Elements", "AcroTextExtractor", "ADelRCP", "AdobeCollabSync", "arh", "FullTrustNotIfier", "LogTransport2", "wow_helper", "outlook", "chrome", "iexplore")
 $appArchitecture = "x86"
+$appProcesses = @("Acrobat", "AcroBroker", "acrobat_sl", "AcroCEF", "acrodist", "acrotray", "AcroRd32", "Acrobat Elements", "AcroTextExtractor", "ADelRCP", "AdobeCollabSync", "arh", "FullTrustNotIfier", "LogTransport2", "wow_helper", "outlook", "chrome", "iexplore")
 $appServices = @("AdobeUpdateService")
-$appTransform = "AcroPro.mst"
-$appSetup = "AcroPro.msi"
+$appTransformURL = "https://github.com/JonathanPitre/Apps/raw/master/Adobe/Acrobat DC/AcroPro.mst"
+$appTransform = Split-Path -Path $appTransformURL -Leaf
 $appInstallParameters = "/QB"
-#$appParameters = "--tool=VolumeSerialize --generate --serial=1016-1899-8440-6413-0576-7429 --leid=V7{}AcrobatCont-12-Win-GM --regsuppress=ss --eulasuppress --stream" #--provfile Optional; path of the folder where prov.xml is created. If this parameter is not specified, prov.xml is created in the folder in which APTEE resides.
 $appAddParameters = "IGNOREVCRT64=1 EULA_ACCEPT=YES UPDATE_MODE=0 DISABLE_ARM_SERVICE_INSTALL=1 ROAMIDENTITY=1 ROAMLICENSING=1"
 $Nevergreen = Get-NevergreenApp -Name AdobeAcrobat | Where-Object {$_.Architecture -eq $appArchitecture}
 $appVersion = $Nevergreen.Version
-$appURLPatch = $Nevergreen.URI
-$appPatch = Split-Path -Path $appURLPatch -Leaf
-$appTransformURL = "https://github.com/JonathanPitre/Apps/raw/master/Adobe/Acrobat DC/AcroPro.mst"
-$appTransform = Split-Path -Path $appTransformURL -Leaf
-$appURLADMX = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/AcrobatADMTemplate.zip"
-$appADMX = Split-Path -Path $appURLADMX -Leaf
-$appURLCustWiz = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/CustWiz2100720091_en_US_DC.exe"
-$appCustWiz = Split-Path -Path $appURLCustWiz -Leaf
+$appSetupURL = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_WWMUI.zip"
+$appSetup = Split-Path -Path $appSetupURL -Leaf
+$appMsiSetup = "AcroPro.msi"
+$appPatchURL = $Nevergreen.URI
+$appPatch = Split-Path -Path $appPatchURL -Leaf
+$appADMXurl = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/AcrobatADMTemplate.zip"
+$appADMX = Split-Path -Path $appADMXurl -Leaf
+$appCustWizURL = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/CustWiz2100720091_en_US_DC.exe"
+$appCustWiz = Split-Path -Path $appCustWizURL -Leaf
 $appCustWizVersion = $appCustWiz.Trim("CustWiz").Trim("_en_US_DC.exe")
 $appDestination = "${env:ProgramFiles(x86)}\$appVendor\$appName $appShortVersion\$appName"
-[boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName $appShortVersion")
-$appInstalledVersion = (Get-InstalledApplication -Name "$appVendor $appName $appShortVersion").DisplayVersion
+[boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName $appShortVersion" -Exact)
+$appInstalledVersion = (Get-InstalledApplication -Name "$appVendor $appName $appShortVersion" -Exact).DisplayVersion
 ##*===============================================
 
 If ([version]$appVersion -gt [version]$appInstalledVersion)
 {
     Set-Location -Path $appScriptDirectory
 
-    # Uninstall previous versions
-    Get-Process -Name $appProcesses | Stop-Process -Force
-    If (($IsAppInstalled) -and (Test-Path -Path $appSetup))
+    # Download latest setup file(s)
+    If (-Not(Test-Path -Path $appScriptDirectory\$appMsiSetup))
     {
-        Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Remove-MSIApplications -Name "$appVendor $appName $appShortVersion"
-    }
-
-    # Download latest policy definitions
-    Write-Log -Message "Downloading $appVendor $appName $appShortVersion ADMX templates..." -Severity 1 -LogType CMTrace -WriteHost $True
-    Invoke-WebRequest -UseBasicParsing -Uri $appURLADMX -OutFile $appADMX
-    New-Folder -Path "$appScriptDirectory\PolicyDefinitions"
-    Expand-Archive -Path $appADMX -DestinationPath "$appScriptDirectory\PolicyDefinitions" -Force
-    Remove-File -Path $appADMX, "$appScriptDirectory\PolicyDefinitions\$($appName)$($appShortVersion).adm"
-
-    # Download latest Adobe Acrobat Customization Wizard DC
-    If (-Not(Test-Path -Path $appScriptDirectory\$appCustWiz))
-    {
-        Write-Log -Message "Downloading $appVendor $appName Custimization Wizard $appShortVersion $appCustWizVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLCustWiz -OutFile $appCustWiz
+        Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appArchitecture $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appSetupURL -OutFile $appSetup
+        Write-Log -Message "Extracting $appVendor $appName $appShortVersion $appArchitecture $appVersion ZIP..." -Severity 1 -LogType CMTrace -WriteHost $True
+        # Extract ZIP
+        Expand-Archive -Path $appScriptDirectory\$appSetup -DestinationPath $appScriptDirectory -Force
+        Copy-File -Path "$appScriptDirectory\$appVendor $appName\*" -Destination $appScriptDirectory -Recurse
+        Remove-Folder -Path "$appScriptDirectory\$appVendor $appName"
+        Remove-File -Path "$appScriptDirectory\$appSetup"
     }
     Else
     {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
-    # Download latest patch file
+    # Download latest patch
     If (-Not(Test-Path -Path $appScriptDirectory\$appPatch))
     {
-        Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appVersion patch..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLPatch -OutFile $appPatch
+        Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appArchitecture $appVersion patch..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appPatchURL -OutFile $appPatch
+        # Modify setup.ini according to latest patch
         If ((Test-Path -Path $appScriptDirectory\$appPatch) -and (Test-Path -Path $appScriptDirectory\$appPatch\setup.ini))
         {
             Set-IniValue -FilePath $appScriptDirectory\setup.ini -Section "Startup" -Key "CmdLine" -Value "/sPB /rs /msi $appAddParameters"
@@ -153,13 +145,39 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
+    # Download latest policy definitions
+    Write-Log -Message "Downloading $appVendor $appName $appShortVersion ADMX templates..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Invoke-WebRequest -UseBasicParsing -Uri $appADMXurl -OutFile $appADMX
+    New-Folder -Path "$appScriptDirectory\PolicyDefinitions"
+    Expand-Archive -Path $appADMX -DestinationPath "$appScriptDirectory\PolicyDefinitions" -Force
+    Remove-File -Path $appADMX, $appScriptDirectory\PolicyDefinitions\*.adm
 
-    If ((Test-Path -Path $appScriptDirectory\$appSetup) -and (Test-Path -Path $appScriptDirectory\$appPatch))
+    # Download latest Adobe Acrobat Customization Wizard DC
+    If (-Not(Test-Path -Path $appScriptDirectory\$appCustWiz))
+    {
+        Write-Log -Message "Downloading $appVendor $appName Custimization Wizard $appShortVersion $appCustWizVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appCustWizURL -OutFile $appCustWiz
+    }
+    Else
+    {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+
+    # Uninstall previous versions
+    Get-Process -Name $appProcesses | Stop-Process -Force
+    If (($IsAppInstalled) -and (Test-Path -Path $appScriptDirectory\$appMsiSetup))
+    {
+        Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Remove-MSIApplications -Name "$appVendor $appName* $appShortVersion*" -WildCard -Exact
+    }
+
+    If ((Test-Path -Path "$appScriptDirectory\$appMsiSetup") -and (Test-Path -Path $appScriptDirectory\$appPatch))
     {
         # Download required transform file
         If (-Not(Test-Path -Path $appScriptDirectory\$appTransform))
         {
-            Write-Log -Message "Downloading $appVendor $appName $appShortVersion Transform..." -Severity 1 -LogType CMTrace -WriteHost $True
+            Write-Log -Message "Downloading $appVendor $appName $appShortVersion $appArchitecture transform..." -Severity 1 -LogType CMTrace -WriteHost $True
             Invoke-WebRequest -UseBasicParsing -Uri $appTransformURL -OutFile $appScriptDirectory\$appTransform
         }
         Else
@@ -168,19 +186,19 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
         }
 
         # Install latest version
-        Write-Log -Message "Installing $appVendor $appName $appShortVersion $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Execute-MSI -Action Install -Path $appSetup -Transform $appTransform -Parameters $appInstallParameters -AddParameters $appAddParameters -Patch $appPatch -SkipMSIAlreadyInstalledCheck
+        Write-Log -Message "Installing $appVendor $appName $appShortVersion $appArchitecture $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Execute-MSI -Action Install -Path $appMsiSetup -Transform $appTransform -Parameters $appInstallParameters -AddParameters $appAddParameters -Patch $appPatch -SkipMSIAlreadyInstalledCheck
     }
     ElseIf (($IsAppInstalled) -and (Test-Path -Path $appScriptDirectory\$appPatch))
     {
         # Install latest patch
-        Write-Log -Message "Setup file(s) are missing, MSP file will be installed." -Severity 1 -LogType CMTrace -WriteHost $True
-        Write-Log -Message "Installing $appVendor $appName $appShortVersion $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Execute-MSP -Path $appPatch -Parameters $appInstallParameters
+        Write-Log -Message "Setup file(s) are missing, MSP file(s) will be installed instead." -Severity 2 -LogType CMTrace -WriteHost $True
+        Write-Log -Message "Installing $appVendor $appName $appShortVersion $appArchitecture $appVersion patch..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Execute-MSP -Path $appPatch
     }
     Else
     {
-        Write-Log -Message "Setup file(s) are missing" -Severity 2 -LogType CMTrace -WriteHost $True
+        Write-Log -Message "Setup file(s) are missing." -Severity 3 -LogType CMTrace -WriteHost $True
         Exit-Script
     }
 
@@ -191,19 +209,12 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     Get-ScheduledTask -TaskName "$appVendor $appName Update Task" | Disable-ScheduledTask
 
     # Stop and disable unneeded services
-    Stop-ServiceAndDependencies -Name $appServices[0]
-    Set-ServiceStartMode -Name $appServices[0] -StartMode "Disabled"
+    Stop-ServiceAndDependencies -Name $appServices[0] -SkipServiceExistsTest -ContinueOnError
+    Set-ServiceStartMode -Name $appServices[0] -StartMode "Disabled" -ContinueOnError
 
     # Remove unneeded applications from running at start-up
-    Remove-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AdobeAAMUpdater-1.0" -ContinueOnError $True
-    #Remove-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AdobeGCInvoker-1.0" -ContinueOnError $True
-
-    <# Needed for serial number install only
-    Execute-Process -Path ".\adobe_prtk.exe" -Parameters $appParameters #logs located in %temp%\AdobeSerialization.log, and %temp%\oobelib.log
-    Execute-Process -Path "$appDestination\Acrobat.exe" -WindowStyle Minimized -NoWait
-    Start-Sleep -s 70
-    Get-Process -Name $appProcesses | Stop-Process -Force
-    #>
+    Remove-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AdobeAAMUpdater-1.0" -ContinueOnError
+    #Remove-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AdobeGCInvoker-1.0" -ContinueOnError
 
     # Remove Active Setup
     Remove-RegistryKey -Key "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Active Setup\Installed Components\{AC76BA86-0000-0000-7760-7E8A45000000}" -Name "StubPath"
@@ -216,9 +227,9 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     # Go back to the parent folder
     Set-Location ..
 
-    Write-Log -Message "$appVendor $appName $appShortVersion $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
+    Write-Log -Message "$appVendor $appName $appShortVersion $appArchitecture $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 }
 Else
 {
-    Write-Log -Message "$appVendor $appName $appShortVersion $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
+    Write-Log -Message "$appVendor $appName $appShortVersion $appArchitecture $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
