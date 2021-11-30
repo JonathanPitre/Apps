7f9be0f4e24c3a2be4836072a5b2fa2d48d4c8e0
Jonathan Pitre <jakeld@gmail.com>
2021-11-30 13:14:41 -0500

    Add VisualCppResdist AIO install scripts

---
+++ VisualCppRedist AIO/Install-All.ps1
@@ -0,0 +1,151 @@
+# Standalone application install script for VDI environment - (C)2021 Jonathan Pitre & Owen Reynolds, inspired by xenappblog.com
+
+#Requires -Version 5.1
+#Requires -RunAsAdministrator
+
+# Custom package providers list
+$PackageProviders = @("Nuget")
+
+# Custom modules list
+$Modules = @("PSADT", "Evergreen")
+
+Write-Verbose -Message "Importing custom modules..." -Verbose
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+
+# Install custom package providers list
+Foreach ($PackageProvider in $PackageProviders)
+{
+    If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
+}
+
+# Add the Powershell Gallery as trusted repository
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+# Update PowerShellGet
+$InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
+$PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
+If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
+
+# Install and import custom modules list
+Foreach ($Module in $Modules)
+{
+    If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
+    Else
+    {
+        $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
+        $ModuleVersion = (Find-Module -Name $Module).Version
+        $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
+        $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
+        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+        {
+            Update-Module -Name $Module -Force
+            Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
+        }
+    }
+}
+
+Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
+
+# Get the current script directory
+Function Get-ScriptDirectory
+{
+    Remove-Variable appScriptDirectory
+    Try
+    {
+        If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
+        ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
+        ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
+        Else
+        {
+            Write-Host -ForegroundColor Red "Cannot resolve script file's path"
+            Exit 1
+        }
+    }
+    Catch
+    {
+        Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
+        Exit 2
+    }
+}
+
+# Variables Declaration
+# Generic
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$env:SEE_MASK_NOZONECHECKS = 1
+$appScriptDirectory = Get-ScriptDirectory
+
+# Application related
+##*===============================================
+Function Get-MicrosoftVisualCPlusPlusVersion
+{
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding()]
+    Param ()
+    $DownloadURL = "https://github.com/abbodi1406/vcredist/releases"
+
+    Try
+    {
+        $DownloadText = (Invoke-WebRequest -Uri $DownloadURL -DisableKeepAlive -UseBasicParsing).RawContent
+    }
+    Catch
+    {
+        Throw "Failed to connect to URL: $DownloadURL with error $_."
+        Break
+    }
+    Finally
+    {
+        $RegEx= "(Visual C\+\+ \d{4} Redistributable) - ((?:\d+\.)+(?:\d+))"
+        $Version = ($DownloadText | Select-String -Pattern $RegExURL).Matches.Groups[2].Value
+    }
+    return $Version
+}
+
+$appVendor = "Microsoft"
+$appName = "VisualCppRedistAIO"
+$appName2 = "Visual C++ 2022"
+$Evergreen = Get-EvergreenApp -Name $appName
+$appVersion = Get-MicrosoftVisualCPlusPlusVersion
+$appVersion = $appVersion.Substring(0, $appVersion.Length - 2)
+$appSetupVersion = $Evergreen.Version
+$appURL = $Evergreen.URI
+$appZip = Split-Path -Path $appURL -Leaf
+$appSetup = "VisualCppRedist_AIO_x86_x64.exe"
+$appInstallParameters = "/aiV"
+[boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName2")
+$appInstalledVersion = (Get-InstalledApplication -Name "$appVendor $appName2").DisplayVersion | Select-Object -First 1
+##*===============================================
+
+If ([version]$appVersion -gt [version]$appInstalledVersion)
+{
+    Set-Location -Path $appScriptDirectory
+    If (-Not(Test-Path -Path $appSetupVersion)) {New-Folder -Path $appSetupVersion}
+    Set-Location -Path $appSetupVersion
+
+    If (-Not(Test-Path -Path $appScriptDirectory\$appSetupVersion\$appSetup))
+    {
+        Write-Log -Message "Downloading $appName $appSetupVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appZip
+        Expand-Archive -Path $appZip -DestinationPath $appScriptDirectory\$appSetupVersion
+        Remove-File -Path $appZip
+    }
+    Else
+    {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    # Install latest version
+    Write-Log -Message "Installing $appName $appSetupVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Execute-Process -Path .\$appSetup -Parameters $appInstallParameters
+
+    # Go back to the parent folder
+    Set-Location ..
+
+    Write-Log -Message "$appName $appSetupVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
+}
+Else
+{
+    Write-Log -Message "$appName $appSetupVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
+}
