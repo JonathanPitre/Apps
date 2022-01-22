ea45077d487e062e66000c5b05f78e19114dbb01
Jonathan Pitre <jakeld@gmail.com>
2022-01-21 20:42:54 -0500

    Update script header

--- ImageGlass/Install.ps1
+++ ImageGlass/Install.ps1
@@ -3,52 +3,15 @@
 #Requires -Version 5.1
 #Requires -RunAsAdministrator
 
-# Custom package providers list
-$PackageProviders = @("Nuget")
-
-# Custom modules list
-$Modules = @("PSADT", "Evergreen")
-
-Write-Verbose -Message "Importing custom modules..." -Verbose
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$env:SEE_MASK_NOZONECHECKS = 1
+$Modules = @("PSADT", "Evergreen") # Modules list
 
-# Install custom package providers list
-Foreach ($PackageProvider in $PackageProviders)
-{
-    If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
-}
-
-# Add the Powershell Gallery as trusted repository
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-
-# Update PowerShellGet
-$InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
-$PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
-If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
-
-# Install and import custom modules list
-Foreach ($Module in $Modules)
-{
-    If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
-    Else
-    {
-        $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
-        $ModuleVersion = (Find-Module -Name $Module).Version
-        $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
-        $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
-        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
-        {
-            Update-Module -Name $Module -Force
-            Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
-        }
-    }
-}
-
-Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
-
-# Get the current script directory
 Function Get-ScriptDirectory
 {
     Remove-Variable appScriptDirectory
@@ -59,26 +22,106 @@ Function Get-ScriptDirectory
         ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
         Else
         {
-            Write-Host -ForegroundColor Red "Cannot resolve script file's path"
+            Write-Host -Object "Cannot resolve script file's path" -ForegroundColor Red
             Exit 1
         }
     }
     Catch
     {
-        Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
+        Write-Host -Object "Caught Exception: $($Error[0].Exception.Message)" -ForegroundColor Red
         Exit 2
     }
 }
 
-# Variables Declaration
-# Generic
-$ProgressPreference = "SilentlyContinue"
-$ErrorActionPreference = "SilentlyContinue"
-$env:SEE_MASK_NOZONECHECKS = 1
+Function Initialize-Module
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$Module
+    )
+    Write-Host -Object  "Importing $Module module..." -ForegroundColor Green
+
+    # If module is imported say that and do nothing
+    If (Get-Module | Where-Object {$_.Name -eq $Module})
+    {
+        Write-Host -Object  "Module $Module is already imported." -ForegroundColor Green
+    }
+    Else
+    {
+        # If module is not imported, but available on disk then import
+        If (Get-Module -ListAvailable | Where-Object {$_.Name -eq $Module})
+        {
+            $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
+            $ModuleVersion = (Find-Module -Name $Module).Version
+            $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
+            $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
+            If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+            {
+                Update-Module -Name $Module -Force
+                Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
+                Write-Host -Object "Module $Module was updated." -ForegroundColor Green
+            }
+            Import-Module -Name $Module -Force -Global -DisableNameChecking
+            Write-Host -Object "Module $Module was imported." -ForegroundColor Green
+        }
+        Else
+        {
+            # Install Nuget
+            If (-not(Get-PackageProvider -ListAvailable -Name NuGet))
+            {
+                Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+                Write-Host -Object "Package provider NuGet was installed." -ForegroundColor Green
+            }
+
+            # Add the Powershell Gallery as trusted repository
+            If ((Get-PSRepository -Name "PSGallery").InstallationPolicy -eq "Untrusted")
+            {
+                Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+                Write-Host -Object "PowerShell Gallery is now a trusted repository." -ForegroundColor Green
+            }
+
+            # Update PowerShellGet
+            $InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
+            $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
+            If ($PSGetVersion -gt $InstalledPSGetVersion)
+            {
+                Install-PackageProvider -Name PowerShellGet -Force
+                Write-Host -Object "PowerShellGet Gallery was updated." -ForegroundColor Green
+            }
+
+            # If module is not imported, not available on disk, but is in online gallery then install and import
+            If (Find-Module -Name $Module | Where-Object {$_.Name -eq $Module})
+            {
+                # Install and import module
+                Install-Module -Name $Module -AllowClobber -Force -Scope AllUsers
+                Import-Module -Name $Module -Force -Global -DisableNameChecking
+                Write-Host -Object "Module $Module was installed and imported." -ForegroundColor Green
+            }
+            Else
+            {
+                # If the module is not imported, not available and not in the online gallery then abort
+                Write-Host -Object "Module $Module was not imported, not available and not in an online gallery, exiting." -ForegroundColor Red
+                EXIT 1
+            }
+        }
+    }
+}
+
+# Get the current script directory
 $appScriptDirectory = Get-ScriptDirectory
 
-# Application related
-##*===============================================
+# Install and import modules list
+Foreach ($Module in $Modules)
+{
+    Initialize-Module -Module $Module
+}
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
 $appName = "ImageGlass"
 $appProcesses = @("ImageGlass")
 $appInstallParameters = "/QB"
@@ -98,7 +141,9 @@ $appURLTheme = "https://github.com/ImageGlass/theme/releases/download/8.2/Colibr
 $appTheme = Split-Path -Path $appURLTheme -Leaf
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appName" -Exact)
 $appInstalledVersion = (Get-InstalledApplication -Name "$appName" -Exact).DisplayVersion
-##*===============================================
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
 
 If ([version]$appVersion -gt [version]$appInstalledVersion)
 {
@@ -107,7 +152,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     Set-Location -Path $appVersion
 
     Write-Log -Message "Uninstalling previous versions..." -Severity 1 -LogType CMTrace -WriteHost $True
-    Remove-MSIApplications -Name $appName -Parameters $appInstallParameters -ContinueOnError
+    Remove-MSIApplications -Name $appName -Parameters $appInstallParameters -ContinueOnError $True
     Get-Process -Name $appProcesses | Stop-Process -Force
 
     # Download latest setup file(s)
@@ -133,13 +178,15 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
-     If (-Not(Test-Path -Path $appScriptDirectory\*.iglang)) {
+    If (-Not(Test-Path -Path $appScriptDirectory\*.iglang))
+    {
         Write-Log -Message "Downloading $appName language file..." -Severity 1 -LogType CMTrace -WriteHost $True
         Invoke-WebRequest -UseBasicParsing -Uri $appURLLang -OutFile "$appScriptDirectory\$appLangZip"
         Expand-Archive -Path $appScriptDirectory\$appLangZip -DestinationPath $appScriptDirectory
         Remove-File -Path $appScriptDirectory\$appLangZip
     }
-    Else {
+    Else
+    {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
@@ -175,7 +222,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     Copy-File -Path $appScriptDirectory\*.igtheme -Destination $appDestination\Themes
 
     # Remove desktop shortcut
-    Remove-File -Path $envCommonDesktop\$appName.lnk -ContinueOnError
+    Remove-File -Path $envCommonDesktop\$appName.lnk -ContinueOnError $True
 
     # Go back to the parent folder
     Set-Location ..
