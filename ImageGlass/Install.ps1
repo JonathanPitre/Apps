b15d6967dc9355b4302c99110f1faa60d3454db1
Jonathan Pitre <jakeld@gmail.com>
2022-01-23 19:51:31 -0500

    Fix configs download

--- ImageGlass/Install.ps1
+++ ImageGlass/Install.ps1
@@ -126,19 +126,19 @@ $appName = "ImageGlass"
 $appProcesses = @("ImageGlass")
 $appInstallParameters = "/QB"
 $appArchitecture = "x64"
-$Evergreen = Get-EvergreenApp $appName | Where-Object { $_.Architecture -eq $appArchitecture }
+$Evergreen = Get-EvergreenApp $appName | Where-Object { $_.Architecture -eq $appArchitecture -and $_.URI -notmatch "deleted"}
 $appVersion = $Evergreen.Version
 $appURL = $Evergreen.URI
 $appSetup = Split-Path -Path $appURL -Leaf
 $appDestination = "$env:ProgramFiles\ImageGlass"
-$appURLLang = "https://api.crowdin.com/api/project/imageglass/download/fr.zip?key=0b08634573c456476345efa8bad174f2"
-$appLangZip = (Split-Path -Path $appURLLang -Leaf).Substring(0, 6)
-$appURLConfigAdmin = "https://raw.githubusercontent.com/JonathanPitre/Apps/master/Image%20Glass/igconfig.admin.xml"
-$appConfigAdmin = Split-Path -Path $appURLConfigAdmin -Leaf
-$appURLConfigDefault = "https://raw.githubusercontent.com/JonathanPitre/Apps/master/Image%20Glass/igconfig.default.xml"
-$appConfigDefault = Split-Path -Path $appURLConfigDefault -Leaf
-$appURLTheme = "https://github.com/ImageGlass/theme/releases/download/8.2/Colibre-24.Amir-H-Jahangard.igtheme"
-$appTheme = Split-Path -Path $appURLTheme -Leaf
+$appLangZipURL = "https://api.crowdin.com/api/project/imageglass/download/fr.zip?key=0b08634573c456476345efa8bad174f2"
+$appLangZip = (Split-Path -Path $appLangZipURL -Leaf).Substring(0, 6)
+$appConfigAdminURL = "https://raw.githubusercontent.com/JonathanPitre/Apps/master/ImageGlass/igconfig.admin.xml"
+$appConfigAdmin = Split-Path -Path $appConfigAdminURL -Leaf
+$appConfigDefaultURL = "https://raw.githubusercontent.com/JonathanPitre/Apps/master/ImageGlass/igconfig.default.xml"
+$appConfigDefault = Split-Path -Path $appConfigDefaultURL -Leaf
+$appThemeURL = "https://github.com/ImageGlass/theme/releases/download/8.2/Colibre-24.Amir-H-Jahangard.igtheme"
+$appTheme = Split-Path -Path $appThemeURL -Leaf
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appName" -Exact)
 $appInstalledVersion = (Get-InstalledApplication -Name "$appName" -Exact).DisplayVersion
 
@@ -170,8 +170,8 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     If (-Not(Test-Path -Path $appScriptDirectory\$appConfigAdmin) -or (-Not(Test-Path -Path $appScriptDirectory\$appConfigDefault)))
     {
         Write-Log -Message "Downloading $appName configuration files.." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLConfigAdmin -OutFile $appScriptDirectory\$appConfigAdmin
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLConfigDefault -OutFile $appScriptDirectory\$appConfigDefault
+        Invoke-WebRequest -UseBasicParsing -Uri $appConfigAdminURL -OutFile $appScriptDirectory\$appConfigAdmin
+        Invoke-WebRequest -UseBasicParsing -Uri $appConfigDefaultURL -OutFile $appScriptDirectory\$appConfigDefault
     }
     Else
     {
@@ -181,7 +181,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     If (-Not(Test-Path -Path $appScriptDirectory\*.iglang))
     {
         Write-Log -Message "Downloading $appName language file..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLLang -OutFile "$appScriptDirectory\$appLangZip"
+        Invoke-WebRequest -UseBasicParsing -Uri $appLangZipURL -OutFile "$appScriptDirectory\$appLangZip"
         Expand-Archive -Path $appScriptDirectory\$appLangZip -DestinationPath $appScriptDirectory
         Remove-File -Path $appScriptDirectory\$appLangZip
     }
@@ -193,7 +193,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     If (-Not(Test-Path -Path $appScriptDirectory\*.igtheme))
     {
         Write-Log -Message "Downloading $appName theme file..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLTheme -OutFile "$appScriptDirectory\$appTheme"
+        Invoke-WebRequest -UseBasicParsing -Uri $appThemeURL -OutFile "$appScriptDirectory\$appTheme"
     }
     Else
     {
@@ -213,13 +213,13 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     Execute-Process -Path "$appDestination\igtasks.exe" -Parameters "regassociations *.b64;*.bay;*.bmp;*.cap;*.cr2;*.crw;*.cur;*.cut;*.dcr;*.dcs;*.dds;*.dib;*.dng;*.drf;*.eip;*.emf;*.erf;*.exif;*.exr;*.fff;*.gif;*.gpr;*.hdr;*.heic;*.ico;*.iiq;*.jfif;*.jpe;*.jpeg;*.jpg;*.jxr;*.k25;*.kdc;*.mdc;*.mef;*.mos;*.mrw;*.nef;*.nrw;*.orf;*.pbm;*.pcx;*.pef;*.pgm;*.png;*.ppm;*.psb;*.psd;*.ptx;*.pxn;*.r3d;*.raf;*.raw;*.rw2;*.rwl;*.rwz;*.sr2;*.srf;*.srw;*.svg;*.tga;*.tif;*.tiff;*.wdp;*.webp;*.wpg;*.x3f;*.xbm" -IgnoreExitCodes
 
     # Install language pack - https://imageglass.org/docs/command-line-utilities
-    Copy-File -Path $appScriptDirectory\*.iglang -Destination $appDestination\Languages
+    Copy-File -Path $appScriptDirectory\*8.5.iglang -Destination $appDestination\Languages
 
     # Copy admin configs - https://imageglass.org/docs/app-configs
     Copy-File -Path $appScriptDirectory\*.xml -Destination $appDestination
 
     # Copy themes files - https://github.com/d2phap/ImageGlass/issues/1112
-    Copy-File -Path $appScriptDirectory\*.igtheme -Destination $appDestination\Themes
+    #Copy-File -Path $appScriptDirectory\*.igtheme -Destination $appDestination\Themes
 
     # Remove desktop shortcut
     Remove-File -Path $envCommonDesktop\$appName.lnk -ContinueOnError $True
