30707c32ad3fc14568d985623313308334c6ae88
Jonathan Pitre <jakeld@gmail.com>
2022-02-17 12:43:41 -0500

    Update script header

--- Citrix/Connection Quality Indicator/Install.ps1
+++ Citrix/Connection Quality Indicator/Install.ps1
@@ -3,52 +3,15 @@
 #Requires -Version 5.1
 #Requires -RunAsAdministrator
 
-# Custom package providers list
-$PackageProviders = @("Nuget")
-
-# Custom modules list
-$Modules = @("PSADT", "Evergreen")
-
-Write-Verbose -Message "Importing custom modules..." -Verbose
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$env:SEE_MASK_NOZONECHECKS = 1
+$Modules = @("PSADT", "Evergreen") # Modules list
 
-# Install custom package providers list
-Foreach ($PackageProvider in $PackageProviders)
-{
-    If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
-}
-
-# Add the Powershell Gallery as trusted repository
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-
-# Update PowerShellGet
-$InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
-$PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
-If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
-
-# Install and import custom modules list
-Foreach ($Module in $Modules)
-{
-    If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
-    Else
-    {
-        $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
-        $ModuleVersion = (Find-Module -Name $Module).Version
-        $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
-        $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
-        If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
-        {
-            Update-Module -Name $Module -Force
-            Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
-        }
-    }
-}
-
-Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
-
-# Get the current script directory
 Function Get-ScriptDirectory
 {
     Remove-Variable appScriptDirectory
@@ -59,106 +22,146 @@ Function Get-ScriptDirectory
         ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
         Else
         {
-            Write-Host -ForegroundColor Red "Cannot resolve script file's path"
+            Write-Host -Object "Cannot resolve script file's path" -ForegroundColor Red
             Exit 1
         }
     }
     Catch
     {
-        Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
+        Write-Host -Object "Caught Exception: $($Error[0].Exception.Message)" -ForegroundColor Red
         Exit 2
     }
 }
 
-# Variables Declaration
-# Generic
-$ProgressPreference = "SilentlyContinue"
-$ErrorActionPreference = "SilentlyContinue"
-$env:SEE_MASK_NOZONECHECKS = 1
-$appScriptDirectory = Get-ScriptDirectory
-
-# Application related
-##*===============================================
-Function Get-CitrixCQI
+Function Initialize-Module
 {
-    [OutputType([System.Management.Automation.PSObject])]
     [CmdletBinding()]
-    Param ()
-    $DownloadURL = "https://support.citrix.com/article/CTX220774"
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$Module
+    )
+    Write-Host -Object  "Importing $Module module..." -ForegroundColor Green
 
-    Try
+    # If module is imported say that and do nothing
+    If (Get-Module | Where-Object {$_.Name -eq $Module})
     {
-        $DownloadText = (Invoke-WebRequest -Uri $DownloadURL -DisableKeepAlive -UseBasicParsing).RawContent
+        Write-Host -Object  "Module $Module is already imported." -ForegroundColor Green
     }
-    Catch
+    Else
     {
-        Throw "Failed to connect to URL: $DownloadURL with error $_."
-        Break
-    }
-    Finally
-    {
-        $RegExVersion = "What's New in ((?:\d+\.)+(?:\d+))"
-        $Version = ($DownloadText | Select-String -Pattern $RegExVersion).Matches.Groups[1].Value
-        $RegExURL = "https.+CitrixCQI\.zip"
-        $URL = ($DownloadText | Select-String -Pattern $RegExURL).Matches.Value
-
-        if ($Version -and $URL)
+        # If module is not imported, but available on disk then import
+        If (Get-Module -ListAvailable | Where-Object {$_.Name -eq $Module})
         {
-            [PSCustomObject]@{
-                Name         = 'Citrix Quality Indicator'
-                Architecture = 'x86'
-                Type         = 'Zip'
-                Version      = $Version
-                Uri          = $URL
+            $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
+            $ModuleVersion = (Find-Module -Name $Module).Version
+            $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
+            $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
+            If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+            {
+                Update-Module -Name $Module -Force
+                Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
+                Write-Host -Object "Module $Module was updated." -ForegroundColor Green
+            }
+            Import-Module -Name $Module -Force -Global -DisableNameChecking
+            Write-Host -Object "Module $Module was imported." -ForegroundColor Green
+        }
+        Else
+        {
+            # Install Nuget
+            If (-not(Get-PackageProvider -ListAvailable -Name NuGet))
+            {
+                Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+                Write-Host -Object "Package provider NuGet was installed." -ForegroundColor Green
+            }
+
+            # Add the Powershell Gallery as trusted repository
+            If ((Get-PSRepository -Name "PSGallery").InstallationPolicy -eq "Untrusted")
+            {
+                Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+                Write-Host -Object "PowerShell Gallery is now a trusted repository." -ForegroundColor Green
+            }
+
+            # Update PowerShellGet
+            $InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
+            $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
+            If ($PSGetVersion -gt $InstalledPSGetVersion)
+            {
+                Install-PackageProvider -Name PowerShellGet -Force
+                Write-Host -Object "PowerShellGet Gallery was updated." -ForegroundColor Green
+            }
+
+            # If module is not imported, not available on disk, but is in online gallery then install and import
+            If (Find-Module -Name $Module | Where-Object {$_.Name -eq $Module})
+            {
+                # Install and import module
+                Install-Module -Name $Module -AllowClobber -Force -Scope AllUsers
+                Import-Module -Name $Module -Force -Global -DisableNameChecking
+                Write-Host -Object "Module $Module was installed and imported." -ForegroundColor Green
+            }
+            Else
+            {
+                # If the module is not imported, not available and not in the online gallery then abort
+                Write-Host -Object "Module $Module was not imported, not available and not in an online gallery, exiting." -ForegroundColor Red
+                EXIT 1
             }
         }
     }
 }
 
+# Get the current script directory
+$appScriptDirectory = Get-ScriptDirectory
+
+# Install and import modules list
+Foreach ($Module in $Modules)
+{
+    Initialize-Module -Module $Module
+}
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
 Function Get-CitrixDownload
 {
     <#
 .SYNOPSIS
-  Downloads a Citrix file from Citrix.com utilizing authentication
+  Downloads a Citrix VDA or ISO from Citrix.com utilizing authentication
 .DESCRIPTION
-  Downloads a Citrix file from Citrix.com utilizing authentication
-.PARAMETER CitrixKB
-  Citrix KB Article number
-.PARAMETER CitrixFile
-  File name to be downloaded
-.PARAMETER FilePath
-  Path to store downloaded file
+  Downloads a Citrix VDA or ISO from Citrix.com utilizing authentication
+  Ryan Butler 2/6/2020 https://github.com/ryancbutler/Citrix/tree/master/XenDesktop/AutoDownload
+.PARAMETER dlNumber
+  Number assigned to binary download
+.PARAMETER dlEXE
+  File to be downloaded
+.PARAMETER dlPath
+  Path to store downloaded file. Must contain following slash (C:\Temp\)
 .PARAMETER CitrixUserName
   Citrix.com username
 .PARAMETER CitrixPassword
   Citrix.com password
 .EXAMPLE
-  Get-CitrixDownload -CitrixKB "220774" -CitrixFile "CitrixCQI.zip" -CitrixUserName "MyCitrixUsername" -CitrixPassword "MyCitrixPassword"
+  Get-CitrixDownload -dlNumber "16834" -dlEXE "Citrix_Virtual_Apps_and_Desktops_7_1912.iso" -CitrixUserName "MyCitrixUsername" -CitrixPassword "MyCitrixPassword" -dlPath "C:\Temp\"
 #>
     Param(
-        [Parameter(Mandatory = $true)]$CitrixKB,
-        [Parameter(Mandatory = $true)]$CitrixFile,
-        [Parameter(Mandatory = $true)]$FilePath,
+        [Parameter(Mandatory = $true)]$dlNumber,
+        [Parameter(Mandatory = $true)]$dlEXE,
+        [Parameter(Mandatory = $true)]$dlPath,
         [Parameter(Mandatory = $true)]$CitrixUserName,
         [Parameter(Mandatory = $true)]$CitrixPassword
     )
     #Initialize Session
-    Invoke-WebRequest "https://identity.citrix.com/Utility/STS/Sign-In" -SessionVariable websession -UseBasicParsing | Out-Null
+    Invoke-WebRequest "https://identity.citrix.com/Utility/STS/Sign-In?ReturnUrl=%2fUtility%2fSTS%2fsaml20%2fpost-binding-response" -SessionVariable websession -UseBasicParsing | Out-Null
 
     #Set Form
     $Form = @{
-        "persistent" = "1"
+        "persistent" = "on"
         "userName"   = $CitrixUserName
-        "loginbtn"   = "Log+in"
         "password"   = $CitrixPassword
-        "returnURL"  = "https://login.citrix.com/bridge?url=https://support.citrix.com/article/CTX${CitrixKB}"
-        "errorURL"   = "https://login.citrix.com?url=https://support.citrix.com/article/CTX${CitrixKB}&err=y"
     }
 
     #Authenticate
     Try
     {
-        Invoke-WebRequest -Uri ("https://identity.citrix.com/Utility/STS/Sign-In") -WebSession $websession -Method POST -Body $Form -ContentType "application/x-www-form-urlencoded" -UseBasicParsing | Out-Null
+        Invoke-WebRequest -Uri ("https://identity.citrix.com/Utility/STS/Sign-In?ReturnUrl=%2fUtility%2fSTS%2fsaml20%2fpost-binding-response") -WebSession $websession -Method POST -Body $form -ContentType "application/x-www-form-urlencoded" -UseBasicParsing -ErrorAction Stop | Out-Null
     }
     Catch
     {
@@ -174,12 +177,23 @@ Function Get-CitrixDownload
         }
     }
 
-    #$OutFile = ($FilePath + $CitrixFile)
-    $OutFile = Join-Path -Path $FilePath -ChildPath $CitrixFile
+    $dlURL = "https://secureportal.citrix.com/Licensing/Downloads/UnrestrictedDL.aspx?DLID=${dlNumber}&URL=https://downloads.citrix.com/${dlNumber}/${dlEXE}"
+    $Download = Invoke-WebRequest -Uri $dlURL -WebSession $WebSession -UseBasicParsing -Method GET
+    $Webform = @{
+        "chkAccept"            = "on"
+        "clbAccept"            = "Accept"
+        "__VIEWSTATEGENERATOR" = ($Download.InputFields | Where-Object { $_.id -eq "__VIEWSTATEGENERATOR" }).value
+        "__VIEWSTATE"          = ($Download.InputFields | Where-Object { $_.id -eq "__VIEWSTATE" }).value
+        "__EVENTVALIDATION"    = ($Download.InputFields | Where-Object { $_.id -eq "__EVENTVALIDATION" }).value
+    }
+
+    $OutFile = ($dlPath + $dlEXE)
     #Download
-    Invoke-WebRequest -WebSession $websession -Uri "https://fileservice.citrix.com/download/secured/support/article/CTX${CitrixKB}/downloads/${CitrixFile}" -OutFile $OutFile -UseBasicParsing
+    Invoke-WebRequest -Uri $dlURL -WebSession $WebSession -Method POST -Body $Webform -ContentType "application/x-www-form-urlencoded" -UseBasicParsing -OutFile $OutFile
     return $OutFile
 }
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
 
 $appVendor = "Citrix"
 $appName = "Connection Quality Indicator"
@@ -194,7 +208,8 @@ $appCitrixKB = "220774"
 $appDestination = "${env:ProgramFiles(x86)}\Citrix\HDX\bin\Connection Quality Indicator"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appVendor $appName")
 $appInstalledVersion = ((Get-InstalledApplication -Name "$appVendor $appName").DisplayVersion)
-##*===============================================
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 If ([version]$appVersion -gt [version]$appInstalledVersion) {
     Set-Location -Path $appScriptDirectory
