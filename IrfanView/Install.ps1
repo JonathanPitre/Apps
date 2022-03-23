6ac1e6cd0ca5f203c8aabee4a0ebc009c150f2a4
Jonathan Pitre <jakeld@gmail.com>
2022-03-23 10:40:53 -0400

    Fix automatic download (Nevergreen PR for IrfanView is required) Update script header

--- IrfanView/Install.ps1
+++ IrfanView/Install.ps1
@@ -3,177 +3,161 @@
 #Requires -Version 5.1
 #Requires -RunAsAdministrator
 
-# Custom package providers list
-$PackageProviders = @("Nuget")
-
-# Custom modules list
-$Modules = @("PSADT", "Evergreen")
-
-Write-Verbose -Message "Importing custom modules..." -Verbose
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$env:SEE_MASK_NOZONECHECKS = 1
+$Modules = @("PSADT", "Nevergreen") # Modules list
 
-# Install custom package providers list
-Foreach ($PackageProvider in $PackageProviders) {
-	If (-not(Get-PackageProvider -ListAvailable -Name $PackageProvider -ErrorAction SilentlyContinue)) { Install-PackageProvider -Name $PackageProvider -Force }
-}
-
-# Add the Powershell Gallery as trusted repository
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-
-# Update PowerShellGet
-$InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
-$PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
-If ($PSGetVersion -gt $InstalledPSGetVersion) { Install-PackageProvider -Name PowerShellGet -Force }
-
-# Install and import custom modules list
-Foreach ($Module in $Modules) {
-	If (-not(Get-Module -ListAvailable -Name $Module)) { Install-Module -Name $Module -AllowClobber -Force | Import-Module -Name $Module -Force }
-	Else {
-		$InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
-		$ModuleVersion = (Find-Module -Name $Module).Version
-		$ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
-		$ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
-		If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion) {
-			Update-Module -Name $Module -Force
-			Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
-		}
-	}
-}
-
-Write-Verbose -Message "Custom modules were successfully imported!" -Verbose
-
-# Get the current script directory
-Function Get-ScriptDirectory {
+Function Get-ScriptDirectory
+{
     Remove-Variable appScriptDirectory
-    Try {
+    Try
+    {
         If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
         ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
         ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
-        Else {
-            Write-Host -ForegroundColor Red "Cannot resolve script file's path"
+        Else
+        {
+            Write-Host -Object "Cannot resolve script file's path" -ForegroundColor Red
             Exit 1
         }
     }
-    Catch {
-        Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
+    Catch
+    {
+        Write-Host -Object "Caught Exception: $($Error[0].Exception.Message)" -ForegroundColor Red
         Exit 2
     }
 }
 
-# Variables Declaration
-# Generic
-$ProgressPreference = "SilentlyContinue"
-$ErrorActionPreference = "SilentlyContinue"
-$env:SEE_MASK_NOZONECHECKS = 1
+Function Initialize-Module
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$Module
+    )
+    Write-Host -Object  "Importing $Module module..." -ForegroundColor Green
+
+    # If module is imported say that and do nothing
+    If (Get-Module | Where-Object {$_.Name -eq $Module})
+    {
+        Write-Host -Object  "Module $Module is already imported." -ForegroundColor Green
+    }
+    Else
+    {
+        # If module is not imported, but available on disk then import
+        If (Get-Module -ListAvailable | Where-Object {$_.Name -eq $Module})
+        {
+            $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
+            $ModuleVersion = (Find-Module -Name $Module).Version
+            $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
+            $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
+            If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+            {
+                Update-Module -Name $Module -Force
+                Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
+                Write-Host -Object "Module $Module was updated." -ForegroundColor Green
+            }
+            Import-Module -Name $Module -Force -Global -DisableNameChecking
+            Write-Host -Object "Module $Module was imported." -ForegroundColor Green
+        }
+        Else
+        {
+            # Install Nuget
+            If (-not(Get-PackageProvider -ListAvailable -Name NuGet))
+            {
+                Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+                Write-Host -Object "Package provider NuGet was installed." -ForegroundColor Green
+            }
+
+            # Add the Powershell Gallery as trusted repository
+            If ((Get-PSRepository -Name "PSGallery").InstallationPolicy -eq "Untrusted")
+            {
+                Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+                Write-Host -Object "PowerShell Gallery is now a trusted repository." -ForegroundColor Green
+            }
+
+            # Update PowerShellGet
+            $InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
+            $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
+            If ($PSGetVersion -gt $InstalledPSGetVersion)
+            {
+                Install-PackageProvider -Name PowerShellGet -Force
+                Write-Host -Object "PowerShellGet Gallery was updated." -ForegroundColor Green
+            }
+
+            # If module is not imported, not available on disk, but is in online gallery then install and import
+            If (Find-Module -Name $Module | Where-Object {$_.Name -eq $Module})
+            {
+                # Install and import module
+                Install-Module -Name $Module -AllowClobber -Force -Scope AllUsers
+                Import-Module -Name $Module -Force -Global -DisableNameChecking
+                Write-Host -Object "Module $Module was installed and imported." -ForegroundColor Green
+            }
+            Else
+            {
+                # If the module is not imported, not available and not in the online gallery then abort
+                Write-Host -Object "Module $Module was not imported, not available and not in an online gallery, exiting." -ForegroundColor Red
+                EXIT 1
+            }
+        }
+    }
+}
+
+# Get the current script directory
 $appScriptDirectory = Get-ScriptDirectory
 
-# Application related
-##*===============================================
-Function Get-IrfanView {
-    <#
-        .NOTES
-            Author: Trond Eirik Haavarstein
-            Twitter: @xenappblog
-    #>
-    [OutputType([System.Management.Automation.PSObject])]
-    [CmdletBinding()]
-    Param()
-        $url = "https://www.irfanview.com/"
-    try {
-        $web = Invoke-WebRequest -UseBasicParsing -Uri $url -ErrorAction SilentlyContinue
-    }
-    catch {
-        Throw "Failed to connect to URL: $url with error $_."
-        Break
-    }
-    finally {
-        $m = $web.ToString() -split "[`r`n]" | Select-String "Version" | Select-Object -First 1
-        $m = $m -replace "<((?!@).)*?>"
-        $m = $m.Replace(' ','')
-        $Version = $m -replace "Version"
-        $File = $Version -replace "\.",""
-        $x32 = "http://download.betanews.com/download/967963863-1/iview$($File)_setup.exe"
-        $x32plugin = "http://download.betanews.com/download/1099412658-1/iview$($File)_plugins_setup.exe"
-        $x64 = "http://download.betanews.com/download/967963863-1/iview$($File)_x64_setup.exe"
-        $x64plugin = "http://download.betanews.com/download/1099412658-1/iview$($File)_plugins_x64_setup.exe"
-        $lang = "https://www.irfanview.net/lang/irfanview_lang_french.exe"
+# Install and import modules list
+Foreach ($Module in $Modules)
+{
+    Initialize-Module -Module $Module
+}
 
-        $PSObjectx32 = [PSCustomObject] @{
-        Version      = $Version
-        Architecture = "x86"
-        Language     = "neutral"
-        URI          = $x32
-        }
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
 
-        $PSObjectx64 = [PSCustomObject] @{
-        Version      = $Version
-        Architecture = "x64"
-        Language     = "neutral"
-        URI          = $x64
-        }
-
-        $PSObjectx32plugin = [PSCustomObject] @{
-        Version            = $Version
-        Architecture       = "x86 Plugin"
-        Language           = "neutral"
-        URI                = $x32plugin
-        }
-
-        $PSObjectx64plugin = [PSCustomObject] @{
-        Version            = $Version
-        Architecture       = "x64 Plugin"
-        Language           = "neutral"
-        URI                = $x64plugin
-        }
-
-        $PSObjectLang = [PSCustomObject] @{
-        Version            = $Version
-        Architecture       = "any"
-        Language           = "french"
-        URI                = $lang
-        }
-
-        Write-Output -InputObject $PSObjectx32
-        Write-Output -InputObject $PSObjectx64
-        Write-Output -InputObject $PSObjectx32plugin
-        Write-Output -InputObject $PSObjectx64plugin
-        Write-Output -InputObject $PSObjectLang
-
-    }
- }
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
 
 $appName = "IrfanView"
 $appProcesses = @("i_view64")
-$appInstallParameters = "/assoc=1 /group=1 /ini=%APPDATA%\IrfanView /silent"
-$Evergreen = Get-EvergreenApp -Name IrfanView | Where-Object {$_.Architecture -eq "x64"}
-$appVersion = $Evergreen.Version
-$appURLSetup = $Evergreen.URI
-$appSetup = $appURLSetup.Split("/")[5]
-$Evergreen = Get-EvergreenApp -Name IrfanView | Where-Object {$_.Architecture -eq "x64 Plugin"}
-$appURLSetupPlugin = $Evergreen.URI
-$appSetupPlugin = $appURLSetupPlugin.Split("/")[5]
-$Evergreen = Get-EvergreenApp -Name IrfanView | Where-Object {$_.Language -eq "french"}
-$appURLSetupLang = $Evergreen.URI
-$appSetupLang = $appURLSetupLang.Split("/")[4]
+$appInstallParameters = "/assoc=1 /group=1 /ini=%APPDATA%\IrfanView /silent /allusers=1"
+$appArchitecture = "x64"
+$appLanguage = "fr"
+$Nevergreen = Get-NevergreenApp -Name $appName | Where-Object {$_.Name -eq $appName -and $_.Architecture -eq $appArchitecture}
+$appVersion = $Nevergreen.Version
+$appURL = $Nevergreen.URI
+$appSetup = Split-Path -Path $appURL -Leaf
+$NevergreenPlugin = Get-NevergreenApp -Name $appName | Where-Object {$_.Name -eq "$appName Plugins" -and $_.Architecture -eq $appArchitecture}
+$appPluginURL = $NevergreenPlugin.URI
+$appPluginSetup = Split-Path -Path $appPluginURL -Leaf
+$NevergreenLang = Get-NevergreenApp -Name $appName | Where-Object {$_.Name -eq "$appName Language" -and $_.Language -eq $appLanguage -and $_.Type -eq "Exe"}
+$appLangSetupURL = $NevergreenLang.URI
+$appSetupLang = Split-Path -Path $appLangSetupURL -Leaf
 $appDestination = "$env:ProgramFiles\IrfanView"
 [boolean]$IsAppInstalled = [boolean](Get-InstalledApplication -Name "$appName")
 $appInstalledVersion = (Get-InstalledApplication -Name "$appName").DisplayVersion
-##*===============================================
 
-If ([version]$appVersion -gt [version]$appInstalledVersion) {
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+If ([version]$appVersion -gt [version]$appInstalledVersion)
+{
     Set-Location -Path $appScriptDirectory
     If (-Not(Test-Path -Path $appVersion)) {New-Folder -Path $appVersion}
     Set-Location -Path $appVersion
 
-    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup)) {
-        Write-Log -Message "Downloading $appVendor $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLSetup -OutFile $appSetup
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLSetupPlugin -OutFile $appSetupPlugin
-        Invoke-WebRequest -UseBasicParsing -Uri $appURLSetupLang -OutFile $appSetupLang
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup))
+    {
+        Write-Log -Message "Downloading $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appSetup
+        Invoke-WebRequest -UseBasicParsing -Uri $appPluginURL -OutFile $appPluginSetup
+        Invoke-WebRequest -UseBasicParsing -Uri $appURLLang -OutFile $appSetupLang
     }
-    Else {
+    Else
+    {
         Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
     }
 
@@ -182,13 +166,13 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
 
     Write-Log -Message "Installing $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
     Execute-Process -Path .\$appSetup -Parameters $appInstallParameters
-    Execute-Process -Path .\$appSetupPlugin -Parameters "/silent"
+    Execute-Process -Path .\$appPluginSetup -Parameters "/silent"
     Execute-Process -Path .\$appSetupLang -NoWait -PassThru
     Start-Sleep -Seconds 3
     Send-Keys -WindowTitle 'IrfanView Language Installer' -Keys "{ENTER}" -WaitSeconds 2
     Send-Keys -WindowTitle 'IrfanView Language Installer' -Keys "{ENTER}"
 
-	Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
     Remove-Folder -Path $envUserStartMenuPrograms\$appName
     New-Shortcut -Path $envCommonStartMenuPrograms\$appName.lnk -TargetPath $appDestination\$appProcesses.exe -IconLocation $appDestination\$appProcesses.exe -Description $appName -WorkingDirectory $appDestination
 
@@ -197,6 +181,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion) {
 
     Write-Log -Message "$appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
 }
-Else {
+Else
+{
     Write-Log -Message "$appName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
 }
