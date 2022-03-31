5b49cbe8a493956ad3d712812c00c95ca1a76d72
Jonathan Pitre <jakeld@gmail.com>
2022-03-31 19:06:47 -0400

    Remove blank lines

--- Microsoft/Azure AMD NvV4 Driver/Install.ps1
+++ Microsoft/Azure AMD NvV4 Driver/Install.ps1
@@ -170,7 +170,6 @@ Function Get-AMDAzureNVv4Driver
 
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
 
-
 $appVendor = "AMD"
 $appVendor2 = "Microsoft"
 $appName = "Software"
