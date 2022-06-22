24f7151417ff8ec2a981bd8153922d92e014fb93
Jonathan Pitre <jakeld@gmail.com>
2022-06-22 14:10:37 -0400

    Add install script

---
+++ Connection Experience Indicator/Install.ps1
@@ -0,0 +1,328 @@
+﻿# Standalone application install script for VDI environment - (C)2022 Jonathan Pitre, inspired by xenappblog.com
+
+#Requires -Version 5.1
+#Requires -RunAsAdministrator
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+# Set the script execution policy for this process
+Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force } Catch {}
+$env:SEE_MASK_NOZONECHECKS = 1
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+$Modules = @("PSADT", "Evergreen") # Modules list
+
+Function Get-ScriptDirectory
+{
+    Remove-Variable appScriptDirectory
+    Try
+    {
+        If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
+        ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
+        ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
+        Else
+        {
+            Write-Host -Object "Cannot resolve script file's path" -ForegroundColor Red
+            Exit 1
+        }
+    }
+    Catch
+    {
+        Write-Host -Object "Caught Exception: $($Error[0].Exception.Message)" -ForegroundColor Red
+        Exit 2
+    }
+}
+
+Function Initialize-Module
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$Module
+    )
+    Write-Host -Object  "Importing $Module module..." -ForegroundColor Green
+
+    # If module is imported say that and do nothing
+    If (Get-Module | Where-Object {$_.Name -eq $Module})
+    {
+        Write-Host -Object  "Module $Module is already imported." -ForegroundColor Green
+    }
+    Else
+    {
+        # If module is not imported, but available on disk then import
+        If (Get-Module -ListAvailable | Where-Object {$_.Name -eq $Module})
+        {
+            $InstalledModuleVersion = (Get-InstalledModule -Name $Module).Version
+            $ModuleVersion = (Find-Module -Name $Module).Version
+            $ModulePath = (Get-InstalledModule -Name $Module).InstalledLocation
+            $ModulePath = (Get-Item -Path $ModulePath).Parent.FullName
+            If ([version]$ModuleVersion -gt [version]$InstalledModuleVersion)
+            {
+                Update-Module -Name $Module -Force
+                Remove-Item -Path $ModulePath\$InstalledModuleVersion -Force -Recurse
+                Write-Host -Object "Module $Module was updated." -ForegroundColor Green
+            }
+            Import-Module -Name $Module -Force -Global -DisableNameChecking
+            Write-Host -Object "Module $Module was imported." -ForegroundColor Green
+        }
+        Else
+        {
+            # Install Nuget
+            If (-not(Get-PackageProvider -ListAvailable -Name NuGet))
+            {
+                Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+                Write-Host -Object "Package provider NuGet was installed." -ForegroundColor Green
+            }
+
+            # Add the Powershell Gallery as trusted repository
+            If ((Get-PSRepository -Name "PSGallery").InstallationPolicy -eq "Untrusted")
+            {
+                Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+                Write-Host -Object "PowerShell Gallery is now a trusted repository." -ForegroundColor Green
+            }
+
+            # Update PowerShellGet
+            $InstalledPSGetVersion = (Get-PackageProvider -Name PowerShellGet).Version
+            $PSGetVersion = [version](Find-PackageProvider -Name PowerShellGet).Version
+            If ($PSGetVersion -gt $InstalledPSGetVersion)
+            {
+                Install-PackageProvider -Name PowerShellGet -Force
+                Write-Host -Object "PowerShellGet Gallery was updated." -ForegroundColor Green
+            }
+
+            # If module is not imported, not available on disk, but is in online gallery then install and import
+            If (Find-Module -Name $Module | Where-Object {$_.Name -eq $Module})
+            {
+                # Install and import module
+                Install-Module -Name $Module -AllowClobber -Force -Scope AllUsers
+                Import-Module -Name $Module -Force -Global -DisableNameChecking
+                Write-Host -Object "Module $Module was installed and imported." -ForegroundColor Green
+            }
+            Else
+            {
+                # If the module is not imported, not available and not in the online gallery then abort
+                Write-Host -Object "Module $Module was not imported, not available and not in an online gallery, exiting." -ForegroundColor Red
+                EXIT 1
+            }
+        }
+    }
+}
+
+# Get the current script directory
+$appScriptDirectory = Get-ScriptDirectory
+
+# Install and import modules list
+Foreach ($Module in $Modules)
+{
+    Initialize-Module -Module $Module
+}
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+Function Resolve-Uri
+{
+    <#
+    .SYNOPSIS
+        Resolves a URI and also returns the filename and last modified date if found.
+
+    .DESCRIPTION
+        Resolves a URI and also returns the filename and last modified date if found.
+
+    .NOTES
+        Site: https://packageology.com
+        Author: Dan Gough
+        Twitter: @packageologist
+
+    .LINK
+        https://github.com/DanGough/Nevergreen
+
+    .PARAMETER Uri
+        The URI resolve. Accepts an array of strings or pipeline input.
+
+    .PARAMETER UserAgent
+        Optional parameter to provide a user agent for Invoke-WebRequest to use. Examples are:
+
+        Googlebot: 'Googlebot/2.1 (+http://www.google.com/bot.html)'
+        Microsoft Edge: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+
+    .EXAMPLE
+        Resolve-Uri -Uri 'http://somewhere.com/somefile.exe'
+
+        Description:
+        Returns the absolute redirected URI, filename and last modified date.
+    #>
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [ValidatePattern('^(http|https)://')]
+        [Alias('Url')]
+        [String[]] $Uri,
+        [Parameter(
+            Mandatory = $false,
+            Position = 1)]
+        [String] $UserAgent
+    )
+
+    begin
+    {
+        $ProgressPreference = 'SilentlyContinue'
+    }
+
+    process
+    {
+
+        foreach ($UriToResolve in $Uri)
+        {
+
+            try
+            {
+
+                $ParamHash = @{
+                    Uri              = $UriToResolve
+                    Method           = 'Head'
+                    UseBasicParsing  = $True
+                    DisableKeepAlive = $True
+                    ErrorAction      = 'Stop'
+                }
+
+                if ($UserAgent)
+                {
+                    $ParamHash.UserAgent = $UserAgent
+                }
+
+                $Response = Invoke-WebRequest @ParamHash
+
+                if ($IsCoreCLR)
+                {
+                    $ResolvedUri = $Response.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+                }
+                else
+                {
+                    $ResolvedUri = $Response.BaseResponse.ResponseUri.AbsoluteUri
+                }
+
+                Write-Verbose "$($MyInvocation.MyCommand): URI resolved to: $ResolvedUri"
+
+                #PowerShell 7 returns each header value as single unit arrays instead of strings which messes with the -match operator coming up, so use Select-Object:
+                $ContentDisposition = $Response.Headers.'Content-Disposition' | Select-Object -First 1
+
+                if ($ContentDisposition -match 'filename="?([^\\/:\*\?"<>\|]+)')
+                {
+                    $FileName = $matches[1]
+                    Write-Verbose "$($MyInvocation.MyCommand): Content-Disposition header found: $ContentDisposition"
+                    Write-Verbose "$($MyInvocation.MyCommand): File name determined from Content-Disposition header: $FileName"
+                }
+                else
+                {
+                    $Slug = [uri]::UnescapeDataString($ResolvedUri.Split('?')[0].Split('/')[-1])
+                    if ($Slug -match '^[^\\/:\*\?"<>\|]+\.[^\\/:\*\?"<>\|]+$')
+                    {
+                        Write-Verbose "$($MyInvocation.MyCommand): URI slug is a valid file name: $FileName"
+                        $FileName = $Slug
+                    }
+                    else
+                    {
+                        $FileName = $null
+                    }
+                }
+
+                try
+                {
+                    $LastModified = [DateTime]($Response.Headers.'Last-Modified' | Select-Object -First 1)
+                    Write-Verbose "$($MyInvocation.MyCommand): Last modified date: $LastModified"
+                }
+                catch
+                {
+                    Write-Verbose "$($MyInvocation.MyCommand): Unable to parse date from last modified header: $($Response.Headers.'Last-Modified')"
+                    $LastModified = $null
+                }
+
+            }
+            catch
+            {
+                Throw "$($MyInvocation.MyCommand): Unable to resolve URI: $($_.Exception.Message)"
+            }
+
+            if ($ResolvedUri)
+            {
+                [PSCustomObject]@{
+                    Uri          = $ResolvedUri
+                    FileName     = $FileName
+                    LastModified = $LastModified
+                }
+            }
+
+        }
+    }
+
+    end
+    {
+    }
+
+}
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+$appName = "Connection Experience Indicator"
+$appProcesses = @("ExperienceIndicator")
+#$Evergreen = Get-EvergreenApp -Name $appName
+$appURL = "https://bit.ly/CEIV13"
+$appURL = Resolve-Uri -Uri $appURL | Select-Object -ExpandProperty URI
+$appZip = (Split-Path -Path $appURL -Leaf).Trim("?ga=1")
+$appVersion = $appZip -match '((?:\d+\.)+(?:\d+))'
+$appVersion = $Matches[0]
+$appSetup = "ExperienceIndicator.exe"
+$appDestination = "$env:ProgramFiles\$appName"
+[boolean]$IsAppInstalled = [boolean](Test-Path -Path "$appDestination\$appSetup")
+$appInstalledVersion = If ($IsAppInstalled) { Get-FileVersion -File "$appDestination\$appSetup" }
+$appInstalledVersion = $appInstalledVersion.Substring(0,$appInstalledVersion.Length-5)
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+If ([version]$appVersion -gt [version]$appInstalledVersion)
+{
+    Set-Location -Path $appScriptDirectory
+    If (-Not(Test-Path -Path $appVersion)) {New-Folder -Path $appVersion}
+    Set-Location -Path $appVersion
+
+    # Download latest setup file(s)
+    If (-Not(Test-Path -Path $appScriptDirectory\$appVersion\$appSetup))
+    {
+        Write-Log -Message "Downloading $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+        Invoke-WebRequest -UseBasicParsing -Uri $appURL -OutFile $appZip
+        Expand-Archive -Path $appZip -DestinationPath "$appScriptDirectory\$appVersion"
+        #Remove-File -Path $appZip
+    }
+    Else
+    {
+        Write-Log -Message "File(s) already exists, download was skipped." -Severity 1 -LogType CMTrace -WriteHost $True
+    }
+
+    # Uninstall previous versions
+    Get-Process -Name $appProcesses | Stop-Process -Force
+
+    # Install latest version
+    Write-Log -Message "Installing $appName $appVersion..." -Severity 1 -LogType CMTrace -WriteHost $True
+    Copy-File -Path .\$appSetup -Destination $appDestination
+
+    Write-Log -Message "Applying customizations..." -Severity 1 -LogType CMTrace -WriteHost $True
+
+    # Configure application shortcut
+    New-Shortcut -Path "$envCommonStartMenuPrograms\Administrative Tools\$appName.lnk" -TargetPath "$appDestination\$appSetup" -IconLocation "$appDestination\$appSetup" -Description "$appName" -WorkingDirectory "$appDestination"
+
+    # Go back to the parent folder
+    Set-Location ..
+
+    Write-Log -Message "$appName $appVersion was installed successfully!" -Severity 1 -LogType CMTrace -WriteHost $True
+}
+Else
+{
+    Write-Log -Message "$appName $appInstalledVersion is already installed." -Severity 1 -LogType CMTrace -WriteHost $True
+}
