6637538e6a2b5f338798e9510dc65c0e194c19cc
Jonathan Pitre <jakeld@gmail.com>
2023-03-07 11:57:53 -0500

    IgnoreLessThanGB set to 100GB since the disk compaction should be used instead Add variables

--- Microsoft/FSLogix Apps/FileServer Maintenance/Start-Invoke-FslShrinkDisk.ps1
+++ Microsoft/FSLogix Apps/FileServer Maintenance/Start-Invoke-FslShrinkDisk.ps1
@@ -1,2 +1,8 @@
-Invoke-Expression "$PSScriptRoot\Invoke-FslShrinkDisk.ps1 -Path E:\FSLogix -Recurse -IgnoreLessThanGB 3 -DeleteOlderThanDays 90 -LogFilePath $PSScriptRoot\Invoke-FslShrinkDisk.csv -ThrottleLimit 20"
+$FSLogixProfilesPath = "D:\FSLogix"
+$IgnoreLessThanGB = "100"
+$DeleteOlderThanDays = "90"
+$LogFilePath = "$PSScriptRoot\Invoke-FslShrinkDisk.csv"
+$ThrottleLimit = "20"
+
+Invoke-Expression "$PSScriptRoot\Invoke-FslShrinkDisk.ps1 -Path $FSLogixProfilesPath -Recurse -IgnoreLessThanGB $IgnoreLessThanGB -DeleteOlderThanDays $DeleteOlderThanDays -LogFilePath $LogFilePath -ThrottleLimit $ThrottleLimit"
 Exit
