80ad308d81bc541cbaf1e751683d13ac9c0ce1c8
Jonathan Pitre <jakeld@gmail.com>
2023-09-20 13:17:52 -0400

    Enable support for EDT Lossy protocol

--- Citrix/Virtual Delivery Agent/Install-ServerMCS-CR.ps1
+++ Citrix/Virtual Delivery Agent/Install-ServerMCS-CR.ps1
@@ -429,8 +429,9 @@ If (($isAppInstalled -eq $false) -and (Test-Path -Path "$appScriptPath\$appVersi
     # Enable new EDT congestion control - https://www.citrix.com/blogs/2023/04/25/turbo-charging-edt-for-unparalleled-experience-in-a-hybrid-world
     Set-RegistryKey -Key "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Wds\icawd\Tds\udp\UDPStackParameters" -Name "edtBBR" -Value "1" -Type "DWord"
 
-    # CVAD 2303 Users stuck on welcome screen when reconnecting to a disconnected session - https://support.citrix.com/article/CTX547782/cvad-2303-users-stuck-on-welcome-screen-when-reconnecting-to-a-disconnected-session
-    #Set-RegistryKey -Key "HKLM:\SOFTWARE\Citrix\Graphics" -Name "PermitRunAsLocalSystem" -Value "1" -Type "DWord"
+    # Enable support for EDT Lossy protocol - https://docs.citrix.com/en-us/citrix-workspace-app-for-windows/ear.html
+    Set-RegistryKey -Key "HKLM:\SOFTWARE\WOW6432Node\Citrix\Audio" -Name "EdtUnreliableAllowed" -Value "1" -Type "DWord"
+
     # Delete logs and cache files
     Remove-File -Path "$env:ProgramData\Citrix\TelemetryService\CitrixAOT\*.etl"
     Remove-File -Path "$env:ProgramData\Citrix\Citrix\VdaCEIP\*.json"
