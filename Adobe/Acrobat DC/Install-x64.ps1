c0c56e8ba5785d4b1d14e877c5f2f27e2cb03e45
Jonathan Pitre <jakeld@gmail.com>
2024-01-11 20:40:24 -0500

    Fix comment

--- Adobe/Acrobat DC/Install-x64.ps1
+++ Adobe/Acrobat DC/Install-x64.ps1
@@ -309,7 +309,7 @@ If ([version]$appVersion -gt [version]$appInstalledVersion)
     Remove-RegistryKey -Key "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Active Setup\Installed Components\{AC76BA86-0000-0000-7760-7E8A45000000}" -Name "StubPath"
 
     # Fix for Z@xxx.tmp files left behind in Temp folder after printing
-    # https://pathandy.com/adobe-temp-files/
+    # https://pathandy.com/adobe-temp-files
     # https://community.adobe.com/t5/acrobat-discussions/what-is-meaning-of-the-setting-tttosysprintdisabled-1-amp-t1tottdisabled-1-when-printing-from/m-p/11670068
     New-Item -Path "$envWinDir" -Name "acroct.ini" -Force
     Set-IniValue -FilePath $envWinDir\acroct.ini -Section "WinFntSvr" -Key "TTToSysPrintDisabled" -Value "1"
